33bc4746a6987d6595da3ff34758bce8c678fffe
pierre <pierre@freepascal.org>
2017-08-03 22:09:52 +0000

    Single script to concentrate all version dependencies
    git-svn-id: @981 -

---
+++ scripts/fpc-versions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export RELEASEVERSION=3.0.2
+export TRUNKDIR=${HOME}/pas/trunk
+export TRUNKVERSION=3.1.1
+export FIXESDIR=${HOME}/pas/fixes
+export FIXESVERSION=3.0.3
+
+FPC_BIN_aarch64=ppca64
+FPC_BIN_arm=ppcarm
+FPC_BIN_avr=ppcavr
+FPC_BIN_i386=ppc386
+FPC_BIN_i8086=ppc8086
+FPC_BIN_jvm=ppcjvm
+FPC_BIN_m68k=ppc68k
+FPC_BIN_mips=ppcmips
+FPC_BIN_mipsel=ppcmipsel
+FPC_BIN_powerpc=ppcppc
+FPC_BIN_powerpc64=ppcppc64
+FPC_BIN_sparc=ppcsparc
+FPC_BIN_x86_64=ppcx64
