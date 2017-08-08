271e2d57a0a4454d41b99af7ab4824b721d8af74
pierre <pierre@freepascal.org>
2017-08-08 17:47:20 +0000

    Add some comments
    git-svn-id: @983 -

--- scripts/fpc-versions.sh
+++ scripts/fpc-versions.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
+#######################################
+## Centralize versions names
+## for Free Pascal installations
+#######################################
+
+#######################################
+## Last Free Pascal Release version
+## Should be installed on the machine
+#######################################
 export RELEASEVERSION=3.0.2
-export TRUNKDIR=${HOME}/pas/trunk
+
+#######################################
+## Free Pascal trunk and fixes versions
+#######################################
 export TRUNKVERSION=3.1.1
-export FIXESDIR=${HOME}/pas/fixes
 export FIXESVERSION=3.0.3
 
+#######################################
+## Free Pascal trunk and fixes
+## directory names 
+#######################################
+export TRUNKDIRNAME=trunk
+export FIXESDIRNAME=fixes
+
+#######################################
+## Free Pascal trunk and fixes versions
+#######################################
+export PASDIR=${HOME}/pas
+export TRUNKDIR=${PASDIR}/${TRUNKDIRNAME}
+export FIXESDIR=${PASDIR}/${FIXESDIRNAME}
+
+#######################################
+## Free Pascal CPU compiler names
+#######################################
 FPC_BIN_aarch64=ppca64
+FPC_BIN_alpha=ppcaxp
 FPC_BIN_arm=ppcarm
 FPC_BIN_avr=ppcavr
 FPC_BIN_i386=ppc386
@@ -17,4 +46,5 @@ FPC_BIN_mipsel=ppcmipsel
 FPC_BIN_powerpc=ppcppc
 FPC_BIN_powerpc64=ppcppc64
 FPC_BIN_sparc=ppcsparc
+FPC_BIN_sparc64=ppcsparc64
 FPC_BIN_x86_64=ppcx64
