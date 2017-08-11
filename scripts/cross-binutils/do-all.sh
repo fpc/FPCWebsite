e428a619e2ef2a9392090391b164c47f7ad3d762
pierre <pierre@freepascal.org>
2017-08-11 16:01:37 +0000

    New script to try to centralize generation of cross-assembler and linker using GNU binutils
    git-svn-id: @986 -

---
+++ scripts/cross-binutils/do-all.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+## aarch64 CPU
+## system_aarch64_darwin,     { 86 }
+./do-one.sh aarch64-linux aarch64-unknown-linux-gnu
+## system_aarch64_linux,      { 88 }
+./do-one.sh aarch64-darwin aarch64-unknown-darwin
+
+## alpha CPU
+## Alpha CPU is obbsolete
+## ./do-one.sh alpha-linux alpha-unknown-linux-gnu
+
+## arm CPU
+## system_arm_android,        { 77 }
+./do-one.sh arm-android arm-unknown-android-gnu
+## system_arm_aros,           { 91 }
+./do-one.sh arm-aros arm-unknown-aros
+## system_arm_darwin,         { 64 }
+./do-one.sh arm-darwin arm-unknown-darwin
+## system_arm_embedded,       { 57 }
+./do-one.sh arm-embedded arm-none-elf
+## system_arm_gba,            { 42 }
+./do-one.sh arm-gba arm-agb-elf
+## system_arm_linux,          { 31 }
+./do-one.sh arm-linux arm-unknown-linux-gnu
+## system_arm_nds,            { 47 }
+./do-one.sh arm-nds arm-unknown-nds
+## system_arm_palmos,         { 45 }
+## binutils 2.14 patched with http://prc-tools.sourceforge.net/
+export BINUTILS_RELEASE="2.14"
+./do-one.sh arm-palmos arm-palmos
+export BINUTILS_RELEASE=
+
+## system_arm_symbian,        { 60 }
+./do-one.sh arm-symbian arm-unknown-symbian
+## system_arm_wince,          { 38 }
+./do-one.sh arm-wince arm-wince-pe
+
+## avr CPU
+## system_avr_embedded,       { 62 }
+./do-one.sh avr-embedded avr-none-elf
+
+## ia64 CPU
+## ia64 CPU is obsolete
+## ./do-one.sh ia64-linux ia64-unknown-linux-gnu
+
+## i386 CPU
+## system_i386_android,       { 78 }
+./do-one.sh i386- i386-unknown-
+## system_i386_aros,          { 83 }
+./do-one.sh i386- i386-unknown-
+## system_i386_beos,          { 16 }
+./do-one.sh i386- i386-unknown-
+## system_i386_darwin,        { 44 }
+./do-one.sh i386-darwin i386-unknown-darwin
+## system_i386_embedded,      { 48 }
+./do-one.sh i386- i386-unknown-
+## system_i386_EMX,           { 28 }
+./do-one.sh i386- i386-unknown-
+## system_i386_freebsd,       { 6 }
+./do-one.sh i386-freebsd i386-unknown-freebsd11
+## system_i386_GO32V2,        { 2 }
+./do-one.sh i386-go32v2 i686-pc-msdosdjgpp
+## system_i386_haiku,         { 63 }
+./do-one.sh i386-haiku i386-unknown-haiku
+## system_i386_iphonesim,     { 69 }
+./do-one.sh i386- i386-unknown-
+## system_i386_linux,         { 3 }
+./do-one.sh i386-linux i386-unknown-linux-gnu
+## system_i386_nativent,      { 68 }
+./do-one.sh i386- i386-unknown-
+## system_i386_netbsd,        { 17 }
+./do-one.sh i386-netbsd i386-unknown-netbsd-gnu
+## system_i386_Netware,       { 19 }
+./do-one.sh i386- i386-unknown-
+## system_i386_netwlibc,      { 35 }
+./do-one.sh i386- i386-unknown-
+## system_i386_openbsd,       { 24 }
+# ./do-one.sh i386-openbsd i386-unknown-openbsd-gnu
+## system_i386_OS2,           { 4 }
+./do-one.sh i386- i386-unknown-
+## system_i386_qnx,           { 20 }
+./do-one.sh i386- i386-unknown-
+## system_i386_solaris,       { 15 }
+./do-one.sh i386-solaris i386-unknown-solaris2.10
+## system_i386_symbian,       { 59 }
+./do-one.sh i386- i386-unknown-
+## system_i386_watcom,        { 32 }
+./do-one.sh i386- i386-unknown-
+## system_i386_wdosx,         { 21 }
+./do-one.sh i386- i386-unknown-
+## system_i386_Win32,         { 5 }
+./do-one.sh i386-win32 i686-w64-mingw32
+## system_i386_wince,         { 40 }
+./do-one.sh i386-wince i686-unknown-mingw32ce
+
+## i8086 CPU
+## GNU assmebler cannot (yet) be
+## used to generate Free Pascal programs
+## system_i8086_embedded,     { 90 }
+## system_i8086_msdos,        { 79 }
+## system_i8086_win16,        { 89 }
+
+
+## JVM pseudo-CPU
+## JVM backend uses java compiler
+## system_jvm_android32,      { 76 }
+## system_jvm_java32,         { 75 }
+
+
+## m68k CPU
+## system_m68k_Amiga,         { 7 }
+./do-one.sh m68k-amiga m68k-unknown-amiga
+## system_m68k_Atari,         { 8 }
+./do-one.sh m68k-atari m68k-unknown-atari
+## system_m68k_embedded,      { 49 }
+./do-one.sh m68k-embedded m68k-none-elf
+## system_m68k_linux,         { 10 }
+./do-one.sh m68k-linux m68k-unknown-linux-gnu
+## system_m68k_Mac,           { 9 }
+./do-one.sh m68k-mac m68k-unknown-macos
+## system_m68k_netbsd,        { 18 }
+./do-one.sh m68k-netbsd m68k-unknown-netbsd-gnu
+## system_m68k_openbsd,       { 25 }
+./do-one.sh m68k-openbsd m68k-unknown-openbsd-gnu
+## system_m68k_PalmOS,        { 11 }
+export BINUTILS_RELEASE="2.14"
+./do-one.sh m68k-palmos m68k-palmos
+export BINUTILS_RELEASE=
+
+## mips CPU
+## system_mipseb_embedded,    { 81 }
+./do-one.sh mips-embedded mips-none-elf
+## system_mipseb_linux,       { 66 }
+./do-one.sh mips-linux mips-unknown-linux-gnu
+## system_mips_embedded,      { 56 }
+## Same as mips_embedded??
+
+## mipsel CPU
+## system_mipsel_android,     { 80 }
+## system_mipsel_embedded,    { 82 }
+./do-one.sh mipsel-embedded mipsel-none-elf
+## system_mipsel_linux,       { 67 }
+./do-one.sh mipsel-linux mipsel-unknown-linux-gnu
+
+## powerpc CPU
+## system_powerpc_aix,        { 73 }
+./do-one.sh powerpc-aix powerpc-unknown-aix
+## system_powerpc_Amiga,      { 36 }
+./do-one.sh powerpc-amiga powerpc-unknown-amiga
+## system_powerpc_darwin,     { 27 }
+./do-one.sh powerpc-darwin powerpc-unknown-darwin
+## system_powerpc_embedded,   { 51 }
+./do-one.sh powerpc-embedded powerpc-none-elf
+## system_powerpc_linux,      { 13 }
+./do-one.sh powerpc-linux powerpc-unknown-linux-gnu
+## system_powerpc_macos,      { 14 }
+./do-one.sh powerpc-macos powerpc-unknown-macos
+## system_powerpc_MorphOS,    { 33 }
+./do-one.sh powerpc-morphos powerpc-unknown-morphos
+## system_powerpc_netbsd,     { 29 }
+./do-one.sh powerpc-netbsd powerpc-unknown-netbsd-gnu
+## system_powerpc_openbsd,    { 30 }
+./do-one.sh powerpc-openbsd powerpc-unknown-openbsd-gnu
+## system_powerpc_wii,        { 70 }
+./do-one.sh powerpc-wii powerpc-eabi
+
+## powerpc64 CPU
+## system_powerpc64_aix,      { 74 }
+./do-one.sh powerpc64-aix powerpc64-unknown-aix
+## system_powerpc64_darwin,   { 46 }
+./do-one.sh powerpc64-darwin powerpc64-unknown-darwin
+## system_powerpc64_embedded, { 58 }
+./do-one.sh powerpc64-embedded powerpc64-none-elf
+## system_powerpc64_linux,    { 43 }
+./do-one.sh powerpc64-linux powerpc64-unknown-linux-gnu
+
+## sparc CPU
+## system_sparc_embedded,     { 52 }
+./do-one.sh sparc-embedded sparc-none-elf
+## system_sparc_linux,        { 23 }
+./do-one.sh sparc-linux sparc-unknown-linux-gnu
+## system_sparc_solaris,      { 22 }
+./do-one.sh sparc-solaris sparc-unknown-solaris2.10
+
+## sparc64 CPU
+## system_sparc64_linux       { 93 }
+./do-one.sh sparc64-linux sparc64-unknown-linux-gnu
+
+## WASM CPU
+## system_wasm_wasm32,        { 92 }
+
+## x86_64 CPU
+## system_x86_6432_linux,     { 41 }
+## system_x86_64_aros,        { 84 }
+## system_x86_64_darwin,      { 61 }
+./do-one.sh x86_64-darwin x86_64-unknown-darwin
+## system_x86_64_dragonfly,   { 85 }
+./do-one.sh x86_64-dragonfly amd64-unknown-dragonflybsd
+## system_x86_64_embedded,    { 55 }
+## system_x86_64_freebsd,     { 34 }
+./do-one.sh x86_64-freebsd amd64-unknown-freebsd11
+## system_x86_64_iphonesim,   { 87 }
+## same as system_x86_64_darwin
+## system_x86_64_linux,       { 26 }
+./do-one.sh x86_64-linux amd64-unknown-linux-gnu
+## system_x86_64_netbsd,      { 72 }
+./do-one.sh x86_64-netbsd amd64-unknown-netbsd-gnu
+## system_x86_64_openbsd,     { 71 }
+#./do-one.sh x86_64-openbsd amd64-unknown-openbsd-gnu
+## system_x86_64_solaris,     { 65 }
+./do-one.sh x86_64-solaris x86_64-unknown-solaris2.10
+## system_x86_64_win64,       { 37 }
+./do-one.sh x86_64-win64 x86_64-w64-mingw32
+
+
+## List was generated using
+## sed -n "s:^ *\(system_.*\)$:## \1:p" ~/pas/trunk/fpcsrc/compiler/systems.inc  | sort
+## system_none,               { 0 }
+
---
+++ scripts/cross-binutils/do-one.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+. $HOME/bin/fpc-versions.sh
+
+# Default binutils version is
+# 2.29 (last version August 2017)
+if [ "X$BINUTILS_RELEASE" == "X" ] ; then
+  BINUTILS_RELEASE=2.29
+fi
+
+# Latest installed Free Pascal release
+# used to install generated cross-assembler/linker
+FPC_RELEASE=$RELEASEVERSION
+
+# Set DO_STRIP if you want to minimize executable size
+if [ "X${DO_STRIP}" == "X" ] ; then
+  DO_STRIP=0
+fi
+
+SOURCE_OS=`uname -o`
+echo "SOURCE_OS=$SOURCE_OS"
+
+# If on Cygwin/Msys system
+# be sure to use i686-w64-mingw32 as host
+if [ "x$SOURCE_OS" == "xCygwin" ] ; then
+  EXEEXT=.exe
+  export HOST=i686-w64-mingw32
+elif [ "x$SOURCE_OS" == "xMsys" ] ; then
+  EXEEXT=.exe
+  export HOST=i686-w64-mingw32
+else
+  EXEEXT=
+fi
+
+# Set fpcbindir to install directory
+if [ "X$fpcbindir" == "X" ] ; then
+  if [[ "x$SOURCE_OS" == "xCygwin" || "x$SOURCE_OS" == "xMsys" ]] ; then
+    if [ "X$HOME" != "X" ] ;then
+      fpcbindir=$HOME/pas/fpc-${FPC_RELEASE}/bin/i386-win32/
+    else
+      fpcbindir=e:/pas/fpc-${FPC_RELEASE}/bin/i386-win32/
+    fi
+    # Link with -static option
+    LDFLAGS="-Wl,-static"
+  else
+    fpcbindir=$HOME/pas/fpc-${FPC_RELEASE}/bin/
+  fi
+fi
+
+# Set CFLAGS variable
+if [ "X$CFLAGS" != "X" ] ; then
+  if [ "$CFLAGS" == " " ]; then
+    LOCAL_CFLAGS=""
+  else
+    LOCAL_CFLAGS=CFLAGS="$CFLAGS"
+  fi
+else
+  LOCAL_CFLAGS=CFLAGS="-gdwarf-4 -O0"
+fi
+
+# Set config_option (might have a non empty startig value)
+if [ "X$HOST" != "X" ] ; then
+  config_option="$config_option --host=$HOST"
+else
+  config_option="$config_option"
+fi
+
+# Usage function
+function usage ()
+{
+  echo "This script requires two parameters"
+  echo "param1 is the Free Pascal like prefix i386-linux"
+  echo "param2 is the GNU binutils configure target option like i386-unknown-linux-gnu"
+}
+
+
+binutilsdir=binutils-${BINUTILS_RELEASE}
+prefix=$1
+target=$2
+
+LOGFILE=../$target.log
+
+# $1 is the file base name
+# $2 can be a "-new" suffix that needs to be removed
+
+function copytofpcbin ()
+{
+  file=$1
+  suff=$2
+  if [ -f ${file}${suff}${EXEEXT} ]; then
+    rm ${file}${suff}${EXEEXT}
+  fi
+  if [ -f .libs/${file}${suff}${EXEEXT} ]; then
+    rm .libs/${file}${suff}${EXEEXT}
+  fi
+  echo "Copying $file to $fpcbindir"
+  make LDFLAGS="$LDFLAGS"
+  if [ -f .libs/${file}${suff}${EXEEXT} ]; then
+    cp .libs/${file}${suff}${EXEEXT} ${fpcbindir}${prefix}-${file}${EXEEXT}
+  else
+    cp ${file}${suff}${EXEEXT} ${fpcbindir}/${prefix}-${file}${EXEEXT}
+  fi
+  if [ $DO_STRIP -eq 1 ] ; then
+    strip -p -s ${fpcbindir}/${prefix}-${file}${EXEEXT}
+  fi
+}
+
+  if [ $# -lt 2 ] ; then
+    usage
+    exit
+  fi
+
+  if [ "$3" == "copy" ]; then
+    copy_only=1
+  fi
+
+  if [ ! -d ../${binutilsdir} ] ; then
+    tarfile=`ls -1 ../*${binutilsdir}* | grep -e ".*tar" -e "*gz" -e "*.bz2" -e "*.xz" | head -1 `
+    if [ ! -f ${tarfile} ] ; then
+      echo "No ${binutilsdir} pacakge found, trying to upload from ftp.gnu.org depository"
+      # Should have all from 2.7 to 2.29 (somaetimes with 'a' suffix
+      wget ftp://ftp.gnu.org/gnu/binutils/binutils-${BINUTILSVERSION}*.tar.bz2 ..
+      res=$?
+      if [ $res -ne 0 ] ; then
+        wget ftp://ftp.gnu.org/gnu/binutils/binutils-${BINUTILSVERSION}*.tar.gz ..
+      fi
+    fi
+    # Try to extract 
+    tar -xvf ../binutils-${BINUTILSVERSION}*.tar
+    res=$?
+    if [ ! -d ../${binutilsdir} ] ; then
+      echo "Failed to upload/untar sources for ${BINUTILSVERSION}"
+      exit 1
+    fi
+  fi
+  if [ ! -d ${prefix} ] ; then
+    echo Creating directory ${prefix}
+    mkdir ${prefix}
+  fi
+
+  cd ${prefix}
+  if [ "$copy_only" == "" ]; then
+    export "$LOCAL_CFLAGS"
+    export LDFLAGS="${LDFLAGS}"
+    ../../${binutilsdir}/configure $config_option --target=$target  --disable-intl --disable-libtool
+    make all-binutils all-gas all-ld | tee $LOGFILE 2>&1
+  fi
+  cd gas
+  copytofpcbin as -new
+
+  cd ../ld
+  copytofpcbin ld -new
+
+  cd ../binutils
+  copytofpcbin  ar
+  copytofpcbin  objdump
+  copytofpcbin  readelf
+
