ddc13d61338ba3a2917758e3f5af8292f4ba4f9c
pierre <pierre@freepascal.org>
2017-08-25 14:08:52 +0000

    * Use freebsd compatible #! frirst line   + Add description for different parts   + Add PASDIR variable for default location of all Free Pascal   + Add INSTALLFPCDIRPREFIX env variable to build version related     INSTALLTRUNKDIR and INSTALLFIXESDIR variables   + Add FPCLOGDIR, defaulting to $HOME/logs   + Add NDK env variable if Android development NDK is installed     to allow cross-compilation for CPU_android targets.
    git-svn-id: @987 -

--- scripts/fpc-versions.sh
+++ scripts/fpc-versions.sh
@@ -1,38 +1,65 @@
-#!/bin/bash
-#######################################
-## Centralize versions names
-## for Free Pascal installations
-#######################################
+#!/usr/bin/env bash
+#################################################################
+## Centralize versions names for Free Pascal installations
+## The easiest way to accomodate is to add symbolic links
+## to your own installation
+#################################################################
 
-#######################################
+#################################################################
 ## Last Free Pascal Release version
 ## Should be installed on the machine
-#######################################
+#################################################################
 export RELEASEVERSION=3.0.2
 
-#######################################
+#################################################################
 ## Free Pascal trunk and fixes versions
-#######################################
+#################################################################
 export TRUNKVERSION=3.1.1
 export FIXESVERSION=3.0.3
 
-#######################################
-## Free Pascal trunk and fixes
-## directory names 
-#######################################
-export TRUNKDIRNAME=trunk
-export FIXESDIRNAME=fixes
+#################################################################
+## Free Pascal trunk and fixes directory names 
+#################################################################
+if [ -z "$TRUNKDIRNAME" ] ; then
+  TRUNKDIRNAME=trunk
+fi
+export TRUNKDIRNAME
+if [ -z "$FIXESDIRNAME" ] ; then
+  FIXESDIRNAME=fixes
+fi 
+export FIXESDIRNAME
 
-#######################################
+#################################################################
 ## Free Pascal trunk and fixes versions
-#######################################
-export PASDIR=${HOME}/pas
+#################################################################
+if [ -z "$PASDIR" ] ; then
+  PASDIR=${HOME}/pas
+fi
+
 export TRUNKDIR=${PASDIR}/${TRUNKDIRNAME}
 export FIXESDIR=${PASDIR}/${FIXESDIRNAME}
 
-#######################################
+#################################################################
+## Free Pascal trunk and fixes installation directories
+#################################################################
+INSTALLFPCDIRPREFIX=${HOME}/pas/fpc-
+INSTALLTRUNKDIR=${INSTALLFPCDIRPREFIX}${TRUNKVERSION}
+INSTALLFIXESDIR=${INSTALLFPCDIRPREFIX}${FIXESVERSION}
+
+#################################################################
+## Free Pascal mainn directory for log files
+#################################################################
+if [ -z "$FPCLOGDIR" ] ; then
+  if [ -d $HOME/logs ] ; then
+    FPCLOGDIR=$HOME/logs
+  elif [ -d $PASDIR/logs ] ; then
+    FPCLOGDIR=$PASDIR/logs
+  fi
+fi
+
+#################################################################
 ## Free Pascal CPU compiler names
-#######################################
+#################################################################
 FPC_BIN_aarch64=ppca64
 FPC_BIN_alpha=ppcaxp
 FPC_BIN_arm=ppcarm
@@ -48,3 +75,18 @@ FPC_BIN_powerpc64=ppcppc64
 FPC_BIN_sparc=ppcsparc
 FPC_BIN_sparc64=ppcsparc64
 FPC_BIN_x86_64=ppcx64
+
+#################################################################
+## NDK must be set for cross-compilation for Android
+#################################################################
+if [ -z "$NDK_VERSION" ] ; then
+  NDK_VERSION=r15c
+fi
+if [ -z "$NDK_BASE_DIR" ] ; then
+  NDK_BASE_DIR=${HOME}/gnu/android-ndk
+fi
+
+if [ -d $NDK_BASE_DIR/android-ndk-$NDK_VERSION ] ; then
+  export ANDROID_NDK_ROOT=$NDK_BASE_DIR/android-ndk-$NDK_VERSION
+  export NDK=$ANDROID_NDK_ROOT
+fi
