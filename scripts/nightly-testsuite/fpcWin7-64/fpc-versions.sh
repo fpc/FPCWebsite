111df0e5685898a5610d96127f3890d4925e7953
pierre <pierre@freepascal.org>
2017-12-04 16:59:51 +0000

    Revert to 3.0.2 for now, until verion check is changed in Makefile's
    git-svn-id: @1029 -

--- scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
+++ scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 export FPCDIR=$HOME/pas
-export RELEASEVERSION=3.0.4
+#export RELEASEVERSION=3.0.4
+# No yet ready for this, needs change to OVERRIDEVERSIONCHECK first
+export RELEASEVERSION=3.0.2
 export TRUNKDIR=$FPCDIR/trunk
 export TRUNKVERSION=3.1.1
 export FIXESDIR=$FPCDIR/fixes
