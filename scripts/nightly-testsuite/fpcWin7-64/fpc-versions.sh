f07f0c1349393d530843820b0c9a75607351db17
pierre <pierre@freepascal.org>
2018-04-04 15:31:41 +0000

    Change to 3.0.4 release
    git-svn-id: @1105 -

--- scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
+++ scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 export FPCDIR=$HOME/pas
-#export RELEASEVERSION=3.0.4
-# No yet ready for this, needs change to OVERRIDEVERSIONCHECK first
-export RELEASEVERSION=3.0.2
+export RELEASEVERSION=3.0.4
 export TRUNKDIR=$FPCDIR/trunk
 export TRUNKVERSION=3.1.1
 export FIXESDIR=$FPCDIR/fixes
