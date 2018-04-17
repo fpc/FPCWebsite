905c777f8aabf2244465d8433c29f47c4e6e9786
pierre <pierre@freepascal.org>
2018-04-17 06:58:07 +0000

    Add gcc68 machine handling and pkill calls
    git-svn-id: @1110 -

--- scripts/nightly-testsuite/gcc67/linux-fpcallup.sh
+++ scripts/nightly-testsuite/gcc67/linux-fpcallup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+pkill  --signal TERM -x -f $HOME/bin/linux-fpccommonup.sh
+pkill  --signal TERM -x -f $HOME/bin/check-all-rtl.sh
+pkill  --signal TERM -f "$HOME/bin/makesnapshot.*.sh"
+pkill  --signal TERM -f "$HOME/bin/test-.*.sh"
+
 # Do the same with ppc386
 export FPCBIN=ppc386
 export FIXES=0
@@ -21,6 +26,17 @@ export FIXES=0
 $HOME/bin/check-all-rtl.sh
 
 if [ "X$HOSTNAME" == "Xgcc67" ] ; then
+  gen_all=1
+  skip_go32v2=0
+  skip_msdos=0
+elif [ "X$HOSTNAME" == "Xgcc68" ] ; then
+  gen_all=1
+  skip_go32v2=0
+  skip_msdos=0
+else
+  gen_all=0
+fi
+if [ $gen_all -eq 1 ] ; then
   $HOME/bin/makesnapshotfixes.sh 
   $HOME/bin/makesnapshottrunk.sh
 
@@ -33,11 +49,15 @@ if [ "X$HOSTNAME" == "Xgcc67" ] ; then
   # For trunk
   SVNDIR=trunk
 
-  # Test msdos 
-  $HOME/bin/test-msdos.sh
+  if [ $skip_msdos -eq 0 ] ; then
+    # Test msdos 
+    $HOME/bin/test-msdos.sh
+  fi
 
-  # Test go32v2
-  $HOME/bin/test-go32v2.sh
+  if [ $skip_go32v2 -eq 0 ] ; then
+    # Test go32v2
+    $HOME/bin/test-go32v2.sh
+  fi
 
   # For fixes
   SVNDIR=fixes
