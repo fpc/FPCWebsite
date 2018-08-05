253d83968125f50fb52398aeacaca592cece0677
pierre <pierre@freepascal.org>
2018-08-05 09:14:43 +0000

    Only write to logfile in case of failure
    git-svn-id: @1159 -

--- scripts/nightly-testsuite/solaris/upload_testsuite_results.sh
+++ scripts/nightly-testsuite/solaris/upload_testsuite_results.sh
@@ -4,13 +4,20 @@ DB_HOST=fpc@www.freepascal.org
 DB_UPLOADDIR=testsuite/incoming
 DB_SSH_EXTRA="-i $HOME/.ssh/old-freepascal"
 
+VERBOSE=0
+if [ "X$1" == "X--verbose" ] ; then
+  VERBOSE=1
+fi
+
 UPLOADDIR=$HOME/logs/to_upload
 LOGFILE=$UPLOADDIR/logfile
 cd $UPLOADDIR
 
 for file in *.tar.gz ; do
   if [ -f "$file" ] ; then
-    echo "`date +%Y-%m-%d-%H:%M` Trying to upload file $file" >> $LOGFILE
+    if [ $VERBOSE -ne 0 ] ; then
+      echo "`date +%Y-%m-%d-%H:%M` Trying to upload file $file" >> $LOGFILE
+    fi
     t_ok=0
     scp -q ${DB_SSH_EXTRA} ${file} ${DB_HOST}:${DB_UPLOADDIR}/${file}.part >> $LOGFILE 2>&1
     resscp=$?
@@ -22,9 +29,16 @@ for file in *.tar.gz ; do
       fi
     fi
     if [ $t_ok -eq 1 ] ; then
-      echo "`date +%Y-%m-%d-%H:%M` Transfer of $file succeeded" >> $LOGFILE
+      if [ $VERBOSE -ne 0 ] ; then
+        echo "`date +%Y-%m-%d-%H:%M` Transfer of $file succeeded" >> $LOGFILE
+      fi
       rm -f $file
     else
+      if [ $VERBOSE -eq 0 ] ; then
+        echo "`date +%Y-%m-%d-%H:%M` Trying to upload file $file" >> $LOGFILE
+      fi
+      echo "`date +%Y-%m-%d-%H:%M` Transfer of file $file failed, resscp=$resscp, resssh=$resssh" >> $LOGFILE
+      # Also to stdout to get an email from cron
       echo "`date +%Y-%m-%d-%H:%M` Transfer of file $file failed, resscp=$resscp, resssh=$resssh"
     fi
   fi
