518b0a1213c33c0a4448cea1fcc6c6643c5640fa
pierre <pierre@freepascal.org>
2018-09-10 11:59:45 +0000

    Add new fixes_3_2 branch test and snapshot generation
    git-svn-id: @1176 -

--- scripts/nightly-testsuite/gcc110/crontab.sh
+++ scripts/nightly-testsuite/gcc110/crontab.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 . ~/bin/fpc-versions.sh
 
+export CURVER=$TRUNKVERSION
+export RELEASEVER=$RELEASEVERSION
+
 . ~/bin/linux64-fpctrunkup.sh
 . ~/bin/linux32-fpctrunkup.sh
+
+export FTP_SNAPSHOT_DIR=$TRUNKDIRNAME
+export FPCSVNDIR=$TRUNKDIR
+
+. ~/bin/makesnapshot-new-powerpc.sh
+. ~/bin/makesnapshot-new-powerpc64.sh
+
+export CURVER=$FIXESVERSION
+export RELEASEVER=$RELEASEVERSION
+
+. ~/bin/linux64-fpctrunkup.sh
+. ~/bin/linux32-fpctrunkup.sh
+
+export FTP_SNAPSHOT_DIR=$FIXESDIRNAME
+export FPCSVNDIR=$FIXESDIR
 
 . ~/bin/makesnapshot-new-powerpc.sh
 . ~/bin/makesnapshot-new-powerpc64.sh
