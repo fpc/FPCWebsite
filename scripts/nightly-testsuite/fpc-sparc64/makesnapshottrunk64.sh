3856dfdd43a2b2eec75a7b84c2c87459256cb047
pierre <pierre@freepascal.org>
2018-09-11 09:42:11 +0000

    Remove FPCCPUOPT=-O- which is not required anymore
    git-svn-id: @1182 -

--- scripts/nightly-testsuite/fpc-sparc64/makesnapshottrunk64.sh
+++ scripts/nightly-testsuite/fpc-sparc64/makesnapshottrunk64.sh
@@ -17,5 +17,5 @@ DISTCLEAN_BEFORE_TESTS=1
 FTPDIR=fpc@ftpmaster.freepascal.org:ftp/snapshot/trunk/sparc64-linux
 export GDBMI=1
 # Disable optimization for now
-export EXTRA_MAKE_OPT="FPCCPUOPT=-O-"
+# export EXTRA_MAKE_OPT="FPCCPUOPT=-O-"
 . $HOME/bin/makesnapshot.sh
