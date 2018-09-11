0d8ad7a1a80b4809d59c652c5ccd170df4985192
pierre <pierre@freepascal.org>
2018-09-11 11:59:09 +0000

    Switch to 3.3.1/3.2.0 for trunk/fixes versions
    git-svn-id: @1186 -

--- scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
+++ scripts/nightly-testsuite/fpcWin7-64/fpc-versions.sh
@@ -2,6 +2,6 @@
 export FPCDIR=$HOME/pas
 export RELEASEVERSION=3.0.4
 export TRUNKDIR=$FPCDIR/trunk
-export TRUNKVERSION=3.1.1
+export TRUNKVERSION=3.3.1
 export FIXESDIR=$FPCDIR/fixes
-export FIXESVERSION=3.0.5
+export FIXESVERSION=3.2.0
