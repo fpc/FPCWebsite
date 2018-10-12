a4add83137021cf7db9dbfd0068b24e124c5d598
pierre <pierre@freepascal.org>
2018-10-12 20:09:43 +0000

    Adapt for use in fixes branch   * Use CURVER instead of FPC_VER
    git-svn-id: @1230 -

--- scripts/nightly-testsuite/gcc110/makesnapshot-new-powerpc64.sh
+++ scripts/nightly-testsuite/gcc110/makesnapshot-new-powerpc64.sh
@@ -22,8 +22,8 @@ GCC_DIR=` gcc -m64 -print-libgcc-file-name | xargs dirname`
 FPC_OS=linux
 FPC_BIN=ppc${FPC_CPUSUF}
 FPC_CPUOS=${FPC_CPU}-${FPC_OS}
-if [ -z "$FPC_VER" ] ; then
-  FPC_VER=$TRUNKVERSION
+if [ -z "$CURVER" ] ; then
+  CURVER=$TRUNKVERSION
 fi
 if [ -z "$FPCSVNDIR" ] ; then
   FPCSVNDIR=$TRUNKDIR
@@ -32,11 +32,11 @@ if [ -z "$FTP_SNAPSHOT_DIR" ] ; then
   FTP_SNAPSHOT_DIR=$TRUNKDIRNAME
 fi
 
-export FPC=~/pas/fpc-${FPC_VER}/bin/${FPC_BIN}
+export FPC=~/pas/fpc-${CURVER}/bin/${FPC_BIN}
 
-if [ "${PATH/${FPC_VER}//}" = "${PATH}" ] ; then
-  echo "Adding ${FPC_VER} binary directory"
-  export PATH=${PATH}:~/pas/fpc-${FPC_VER}/bin
+if [ "${PATH/${CURVER}//}" = "${PATH}" ] ; then
+  echo "Adding ${CURVER} binary directory"
+  export PATH=${PATH}:~/pas/fpc-${CURVER}/bin
 fi
 
 echo "Adding $HOME/bin to front of PATH"
@@ -48,10 +48,13 @@ MAKE_OPTIONS="distclean singlezipinstall SNAPSHOT=1 NOGDB=1 DEBUG=1 NOWPOCYCLE=1
 date=`date +%Y-%m-%d`
 
 
+CURFULLVER=`$FPC_BIN -iW`
+
 cd $FPCSVNDIR
+FPC_BRANCH=$FPC_SNAPSHOT_DIR
 
 cat > README-${FPC_CPUOS} <<EOF
-This snapshot was generated ${date} using:
+This ${FPC_BRANCH} snapshot was generated ${date} using:
 ${MAKE} ${MAKE_OPTIONS} OPT="${OPTS}"
 started using ${FPC}
 ${FPC_BIN} -iVDW output is: `${FPC_BIN} -iVDW`
@@ -69,14 +72,14 @@ Pierre Muller
 EOF
 
 
-TAR=./fpc-${FPC_VER}.${FPC_CPUOS}.tar.gz
+TAR=./fpc-${CURFULLVER}.${FPC_CPUOS}.tar.gz
 
 if [ -f ${TAR} ] ; then
   mv -f  ${TAR} ${TAR}.old
 fi
 
-echo ${MAKE} ${MAKE_OPTIONS} OPT="${OPTS}" "|" tee makesnapshot-${date}.txt
-${MAKE} ${MAKE_OPTIONS} OPT="${OPTS}" > makesnapshot-${FPC_CPUOS}-${date}.txt 2>&1
+echo ${MAKE} ${MAKE_OPTIONS} OPT="${OPTS}" "|" tee makesnapshot-${FPC_BRANCH}-${date}.txt
+${MAKE} ${MAKE_OPTIONS} OPT="${OPTS}" > makesnapshot-${FPC_CPUOS}-${FPC_BRANCH}-${date}.txt 2>&1
 
 if [ -f ${TAR} ]; then
   scp ${TAR} README-${FPC_CPUOS} fpcftp:ftp/snapshot/$FTP_SNAPSHOT_DIR/${FPC_CPUOS}
