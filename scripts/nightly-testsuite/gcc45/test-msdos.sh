ae60f303c4088fb7d6c25aa88003b6e70fe4e0e0
pierre <pierre@freepascal.org>
2018-10-14 14:28:28 +0000

    + Add timestamps to log   * Change log directory   + Add -Wh and -dTEST_SLS tests   + Add email message
    git-svn-id: @1235 -

--- scripts/nightly-testsuite/gcc45/test-msdos.sh
+++ scripts/nightly-testsuite/gcc45/test-msdos.sh
@@ -6,6 +6,10 @@ if [ "X$SVNDIR" == "X" ] ; then
   SVNDIR=trunk
 fi
 
+if [ -z "$HOSTNAME" ] ; then
+  HOSTNAME=`uname -n`
+fi
+
 FPCDIR=$HOME/pas/$SVNDIR/fpcsrc
 
 # Add $HOME/bin to PATH variable
@@ -48,6 +52,82 @@ cd $FPCDIR/tests/
 ulimit -m 256000
 #ulimit -v 256000
 ulimit -d 256000
+TODAY=`date +%Y-%m-%d`
+logdir=$FPCLOGDIR/$SVNDIR/$TODAY/msdos
+mkdir -p $logdir
+GLOBAL_LOGFILE=$logdir/test-msdos.log
+
+function display_time ()
+{
+  echo -n "$* "
+  date "+%Y-%m-%d-%H:%M"
+}
+
+function prepare_msdos ()
+{
+if [ "X$1" != "X" ] ; then
+  CROSSOPT="$1"
+else
+  CROSSOPT=
+fi
+
+DIR_OPT=${CROSSOPT// /_}
+LOGFILE=$logdir/prepare-msdos-${DIR_OPT}.log
+display_time "Starting run_one_model with \$TEST_OPT=\"$TEST_OPT\", logfile=$LOGFILE"
+
+(
+display_time "Recompiling native RTL"
+make -C $FPCDIR/rtl distclean all OPT="-n -g"
+res=$?
+if [ $res -ne 0 ] ; then
+  display_time "Recompiling native RTL failed, res =$res"
+  exit
+fi
+display_time "Recompiling native compiler"
+make -C $FPCDIR/compiler distclean cycle OPT="-n -g"
+res=$?
+if [ $res -ne 0 ] ; then
+  display_time "Recompiling native compiler failed, res=$res"
+  exit
+else
+  if [ ! -e $FPCDIR/compiler/$NATIVEPP ] ; then
+     display_time "No $NATIVEPP compiler generated"
+     exit
+  else
+     cp $FPCDIR/compiler/$NATIVEPP $NEWNATIVEFPC
+  fi
+fi
+# This is needed to have an up-to-date fpcmake binary
+display_time "Recompiling native packages and utils"
+make -C $FPCDIR/packages distclean all OPT="-n -g" FPC=$NEWNATIVEFPC
+res=$?
+if [ $res -ne 0 ] ; then
+  display_time "Recompiling native packages failed, res=$res"
+  exit
+fi
+make -C $FPCDIR/utils distclean all OPT="-n -g" FPC=$NEWNATIVEFPC
+res=$?
+if [ $res -ne 0 ] ; then
+  display_time "Recompiling native utils failed, res=$res"
+  exit
+fi
+
+display_time "Recompiling i8086 compiler"
+make -C $FPCDIR/compiler distclean i8086 OPT="-n -g $CROSSOPT" FPC=$NEWNATIVEFPC
+res=$?
+if [ $res -ne 0 ] ; then
+  display_time "Recompiling i8086 compiler failed, res=$res"
+  exit
+else
+  if [ ! -e $FPCDIR/compiler/$CROSSPP ] ; then
+     display_time "No $CROSSPP compiler generated"
+     exit
+  else
+     cp $FPCDIR/compiler/$CROSSPP $NEWCROSSFPC
+  fi
+fi
+) > $LOGFILE 2>&1
+}
 
 function run_one_model ()
 {
@@ -57,90 +137,42 @@ else
   MEMMODEL=Large
 fi
 
-TEST_OPT="-Wm$MEMMODEL $MSDOSOPT"
+TEST_OPT="-Wm$MEMMODEL $2 $MSDOSOPT"
 
-TODAY=`date +%Y-%m-%d`
 DIR_OPT=${TEST_OPT// /_}
-logdir=$FPCLOGDIR/$SVNDIR/$TODAY/msdos/opt$DIR_OPT
-mkdir -p $logdir
 LOGFILE=$logdir/msdos-${DIR_OPT}.log
 
 (
-echo "Recompiling native RTL"
-make -C $FPCDIR/rtl distclean all OPT="-n -g"
-res=$?
-if [ $res -ne 0 ] ; then
-  echo "Recompiling native RTL failed, res =$res"
-  exit
-fi
-echo "Recompiling native compiler"
-make -C $FPCDIR/compiler distclean cycle OPT="-n -g"
-res=$?
-if [ $res -ne 0 ] ; then
-  echo "Recompiling native compiler failed, res=$res"
-  exit
-else
-  if [ ! -e $FPCDIR/compiler/$NATIVEPP ] ; then
-     echo "No $NATIVEPP compiler generated"
-     exit
-  else
-     cp $FPCDIR/compiler/$NATIVEPP $NEWNATIVEFPC
-  fi
-fi
-# This is needed to have an up-to-date fpcmake binary
-echo "Recompiling native packages and utils"
-make -C $FPCDIR/packages distclean all OPT="-n -g" FPC=$NEWNATIVEFPC
-res=$?
-if [ $res -ne 0 ] ; then
-  echo "Recompiling native packages failed, res=$res"
-  exit
-fi
-make -C $FPCDIR/utils distclean all OPT="-n -g" FPC=$NEWNATIVEFPC
-res=$?
-if [ $res -ne 0 ] ; then
-  echo "Recompiling native utils failed, res=$res"
-  exit
-fi
-
-echo "Recompiling i8086 compiler"
-make -C $FPCDIR/compiler distclean i8086 OPT="-n -g" FPC=$NEWNATIVEFPC
-res=$?
-if [ $res -ne 0 ] ; then
-  echo "Recompiling i8086 compiler failed, res=$res"
-  exit
-else
-  if [ ! -e $FPCDIR/compiler/$CROSSPP ] ; then
-     echo "No $CROSSPP compiler generated"
-     exit
-  else
-     cp $FPCDIR/compiler/$CROSSPP $NEWCROSSFPC
-  fi
-fi
-
-echo "Clearing RTL/Packages for msdos"
+display_time "Clearing RTL/Packages for msdos"
 make -C $FPCDIR/rtl clean FPC=$NEWCROSSFPC
 make -C $FPCDIR/packages clean FPC=$NEWCROSSFPC
 
-echo "Compiling dosboxwrapper"
+display_time "Compiling dosboxwrapper"
 make -C $FPCDIR/tests/utils distclean all dosbox/dosbox_wrapper FPC=$NEWNATIVEFPC
 res=$?
 if [ $res -ne 0 ] ; then
-  echo "Recompiling dosbox_wrapper failed, res=$res"
+  display_time "Recompiling dosbox_wrapper failed, res=$res"
   exit
 else
   if [ ! -e $FPCDIR/tests/utils/dosbox/dosbox_wrapper ] ; then
-    echo "No dosbox_wrapper executable file found"
+    display_time "No dosbox_wrapper executable file found"
     exit
   fi
 fi
-echo "Running msdos testsuite for memory model $1"
-make distclean full FPCFPMAKE=$NEWNATIVEFPC TEST_FPC=$NEWCROSSFPC TEST_OPT="$TEST_OPT" TEST_USER=pierre EMULATOR=$FPCDIR/tests/utils/dosbox/dosbox_wrapper
+display_time "Running msdos testsuite for memory model $1"
+make distclean testprep FPCFPMAKE=$NEWNATIVEFPC TEST_FPC=$NEWCROSSFPC TEST_OPT="$TEST_OPT" TEST_USER=pierre EMULATOR=$FPCDIR/tests/utils/dosbox/dosbox_wrapper
 res=$?
+if [ $res -eq 0 ] ; then
+  MAKEFULLOPT=-j3
+  make $MAKEFULLOPT full FPCFPMAKE=$NEWNATIVEFPC TEST_FPC=$NEWCROSSFPC TEST_OPT="$TEST_OPT" TEST_USER=pierre EMULATOR=$FPCDIR/tests/utils/dosbox/dosbox_wrapper
+  res=$?
+  MAKEFULLOPT=
+fi
 if [ $res -ne 0 ] ; then
-  echo "Running testsuite failed, res=$res"
+  display_time "Running testsuite failed, res=$res"
   exit
 else
-  make uploadrun FPCFPMAKE=$NEWNATIVEFPC TEST_FPC=$NEWCROSSFPC TEST_OPT="-Wm$MEMMODEL $MSDOSOPT" TEST_USER=pierre EMULATOR=$FPCDIR/tests/utils/dosbox/dosbox_wrapper
+  make uploadrun FPCFPMAKE=$NEWNATIVEFPC TEST_FPC=$NEWCROSSFPC TEST_OPT="$TEST_OPT" TEST_USER=pierre EMULATOR=$FPCDIR/tests/utils/dosbox/dosbox_wrapper
 fi
 ) > $LOGFILE 2>&1
 }
@@ -148,10 +180,37 @@ fi
 if [ "X$1" != "X" ] ; then
   run_one_model $1
 else
+  (
+  prepare_msdos
   run_one_model tiny
   run_one_model small
   run_one_model medium
   run_one_model compact
   run_one_model large
   run_one_model huge
+  run_one_model medium "-Wh"
+  run_one_model large "-Wh"
+  run_one_model huge "-Wh"
+  # Change ppc386 compiler to use smartlinked sections
+  #LOGFILE=$logdir/msdos-comp-smartlink-sections.log
+  #make -C ../compiler clean i8086 OPT="-n -gsl -dI8086_SMARTLINK_SECTIONS -di8086_link_intern_debuginfo" FPC=${NEWNATIVEFPC} > $LOGFILE 2>&1
+  prepare_msdos " -gsl -dI8086_SMARTLINK_SECTIONS -di8086_link_intern_debuginfo"
+  NEWCROSSFPC=$FPCDIR/compiler/${CROSSPP}sls-safe
+  cp ../compiler/ppc8086 ${NEWCROSSFPC}
+  MSDOSOPT=""
+  run_one_model tiny "-dTEST_SLS"
+  run_one_model small "-dTEST_SLS"
+  run_one_model medium "-dTEST_SLS"
+  run_one_model compact "-dTEST_SLS"
+  run_one_model large "-dTEST_SLS"
+  run_one_model huge "-dTEST_SLS"
+  run_one_model medium "-Wh -dTEST_SLS"
+  run_one_model large "-Wh -dTEST_SLS"
+  run_one_model huge "-Wh -dTEST_SLS"
+  ) > $GLOBAL_LOGFILE 2>&1
+  Build_version=`$NEWCROSSFPC -iV 2> /dev/null`
+  Build_date=`$NEWCROSSFPC -iD 2> /dev/null`
+  mutt -x -s "Free Pascal results for msdos on ${HOSTNAME}, ${Build_version} ${Build_date}" \
+     -i $GLOBAL_LOGFILE -- pierre@freepascal.org < /dev/null
+
 fi
