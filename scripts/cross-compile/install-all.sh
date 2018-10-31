12504d2f5dab9f4b90f3dfcdf3dbfcd2f7352730
pierre <pierre@freepascal.org>
2018-10-31 10:50:36 +0000

    Try to avoid unneeded work:  + Add condition to compilation of trunk and fixes branch:    disabled by default, change do_update to 1.  * Only run do-all.sh script if ~/gnu/binutils/build directory does not exist yet  * Only compile nasm if ~/bin/nasm does not exist  * Use scripts for CPU-darwin-clang and CPU-iphonesim-clang    with --target option.  * Only compile vasm if ~/bin/vasmm68k_std does not exist  + Add vlink compilation
    git-svn-id: @1250 -

--- scripts/cross-compile/install-all.sh
+++ scripts/cross-compile/install-all.sh
@@ -52,7 +52,8 @@ maybe_add_symlink ~/scripts/nightly-testsuite/x86_64-linux/makesnapshottrunk.sh
 QEMU_VERSION=3.0.0
 NASM_VERSION=2.13.03
 VASM_VERSION=1_8d
-
+VLINK_VERSION=0_16a
+do_update=0
 
 cd $HOME/pas
 
@@ -103,21 +104,23 @@ else
   cd ..
 fi
 
+if [ $do_update -eq 1 ] ; then
+  cd $HOME/pas/trunk/fpcsrc/compiler/
+  make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppc386
+  make installsymlink FPC=`pwd`/ppc386 PREFIX=~/pas/fpc-$TRUNKVERSION
+  make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppcx64
+  make installsymlink FPC=`pwd`/ppcx64 PREFIX=~/pas/fpc-$TRUNKVERSION
+  cd ${HOME}/pas/fixesfpcsrc/compiler/
+  make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppc386
+  make installsymlink FPC=`pwd`/ppc386 PREFIX=~/pas/fpc-3.2.0-beta
+  make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppcx64
+  make installsymlink FPC=`pwd`/ppcx64 PREFIX=~/pas/fpc-3.2.0-beta
+  cd $HOME/pas
+  if [ ! -d fpc-3.2.0 ] ; then
+    ln -s fpc-3.2.0-beta fpc-3.2.0
+  fi
+fi
 
-cd trunk/fpcsrc/compiler/
-make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppc386
-make installsymlink FPC=`pwd`/ppc386 PREFIX=~/pas/fpc-$TRUNKVERSION
-make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppcx64
-make installsymlink FPC=`pwd`/ppcx64 PREFIX=~/pas/fpc-$TRUNKVERSION
-cd ~/pas
-cd fixes/
-cd fpcsrc/compiler/
-make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppc386
-make installsymlink FPC=`pwd`/ppc386 PREFIX=~/pas/fpc-3.2.0-beta
-make cycle OPT="-n -gl" FPC=~/pas/fpc-$RELEASEVERSION/bin/ppcx64
-make installsymlink FPC=`pwd`/ppcx64 PREFIX=~/pas/fpc-3.2.0-beta
-cd $HOME/pas
-ln -s fpc-3.2.0-beta fpc-3.2.0
 # install home mutt script 
 cd
 if [ ! -d mutt ] ; then
@@ -129,82 +132,134 @@ if [ ! -d gnu ] ; then
 fi
 cd gnu
 
-if [ ! -d qemu ] ; then
-  mkdir qemu
-fi
-cd qemu
+if [ ! -f $HOME/sys-root/bin/qemu-arm ] ; then
+  if [ ! -d qemu ] ; then
+    mkdir qemu
+  fi
+  cd qemu
 
-# Install qemu
-wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
-tar xvJf qemu-$QEMU_VERSION.tar.xz
-cd qemu-$QEMU_VERSION
-./configure --prefix=$HOME/sys-root
-make all install
+  # Install qemu
+  wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
+  tar xvJf qemu-$QEMU_VERSION.tar.xz
+  cd qemu-$QEMU_VERSION
+  ./configure --prefix=$HOME/sys-root
+  make all install
+fi
 
 # Install cross-binutils
-cd $HOME/gnu
-if [ ! -d binutils ] ; then
-  mkdir binutils
+if [ ! -d $HOME/gnu/binutils/build ] ; then
+  cd $HOME/gnu
+  if [ ! -d binutils ] ; then
+    mkdir binutils
+  fi
+  cd binutils
+  if [ ! -d build ] ; then
+    mkdir build
+  fi
+  cd build
+  ln -s ~/scripts/cross-binutils/do-all.sh
+  ln -s ~/scripts/cross-binutils/do-one.sh
+  screen  ./do-all.sh
 fi
-cd binutils
-if [ ! -d build ] ; then
-  mkdir build
-fi
-cd build
-ln -s ~/scripts/cross-binutils/do-all.sh
-ln -s ~/scripts/cross-binutils/do-one.sh
-screen  ./do-all.sh
 
+if [ ! -f $HOME/bin/nasm ] ; then
+  cd $HOME/gnu
+  if [ ! -d nasm ] ; then
+    mkdir nasm
+  fi
 
-cd gnu
-if [ ! -d nasm ] ; then
-  mkdir nasm
+ cd nasm
+
+  wget https://www.nasm.us/pub/nasm/releasebuilds/$NASM_VERSION/nasm-$NASM_VERSION.tar.gz
+  tar -xvzf nasm-$NASM_VERSION.tar.gz
+  cd nasm-$NASM_VERSION
+  ./configure --prefix=$HOME/gnu
+  make all install
+  cd $HOME/bin
+  ln -s $HOME/gnu/bin/nasm nasm
+  ln -s nasm i8086-msdos-nasm
+  ln -s nasm i8086-embedded-nasm
+  ln -s nasm i8086-win32-nasm
+  ln -s nasm i386-go32v2-nasm
+  ln -s nasm i386-win32-nasm
+  ln -s nasm i386-linux-nasm
+  ln -s nasm i386-embedded-nasm
 fi
-cd nasm
-wget https://www.nasm.us/pub/nasm/releasebuilds/$NASM_VERSION/nasm-$NASM_VERSION.tar.gz
-tar -xvzf nasm-$NASM_VERSION.tar.gz
-cd nasm-$NASM_VERSION
-./configure --prefix=$HOME/gnu
-make all install
-cd
-cd bin
-ln -s $HOME/gnu/bin/nasm nasm
-ln -s nasm i8086-msdos-nasm
-ln -s nasm i8086-embedded-nasm
-ln -s nasm i8086-win32-nasm
-ln -s nasm i386-go32v2-nasm
-ln -s nasm i386-win32-nasm
-ln -s nasm i386-linux-nasm
-ln -s nasm i386-embedded-nasm
 
 # Add clang symlinks
-if [ -n "`which clang`" ] ; then
-  ln -s `which clang`
-  ln -s clang i386-darwin-clang
-  ln -s clang aarch64-darwin-clang
-  ln -s clang arm-darwin-clang
-  ln -s clang x86_64-darwin-clang
-  ln -s clang powerpc64-darwin-clang
-  ln -s clang powerpc-darwin-clang
-  ln -s clang arm-iphonesim-clang
-  ln -s clang aarch64-iphonesim-clang
-  ln -s clang i386-iphonesim-clang
-  ln -s clang x86_64-iphonesim-clang
+if [ ! -f $HOME/bin/clang ] ; then
+  cd $HOME/bin
+  if [ -n "`which clang`" ] ; then
+   ln -s `which clang`
+cat /home/muller/bin/aarch64-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=aarch64-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/aarch64-iphonesim-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=aarch64-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/arm-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=arm-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/arm-iphonesim-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=arm-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/i386-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=i386-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/i386-iphonesim-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=i386-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/powerpc64-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=powerpc64-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/powerpc-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=powerpc-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/x86_64-darwin-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=x86_64-apple-darwin-macho $*
+HERE_SCRIPT
+cat /home/muller/bin/x86_64-iphonesim-clang <<HERE_SCRIPT
+#!/usr/bin/env bash
+clang$CLANG_VERSION --target=x86_64-apple-darwin-macho $*
+HERE_SCRIPT
+  fi
 fi
 
 # Add vasm assembler
-cd
-cd gnu
-mkdir vasm
-cd vasm
+if [ ! -f $HOME/bin/vasmm68k_std ] ; then
+  cd $HOME/gnu
+  mkdir vasm
+  cd vasm
 
-wget http://server.owl.de/~frank/tags/vasm${VASM_VERSION}.tar.gz
-tar -xvzf vasm${VASM_VERSION}.tar.gz 
-cd vasm
-make CPU=m68k SYNTAX=mot
-make CPU=m68k SYNTAX=std
-make CPU=x86 SYNTAX=std
-make CPU=ppc SYNTAX=std
-make CPU=arm SYNTAX=std
-cp vasmm68k_mot vobjdump vasmm68k_std vasmx86_std vasmppc_std vasmarm_std ~/bin
+  wget http://server.owl.de/~frank/tags/vasm${VASM_VERSION}.tar.gz
+  tar -xvzf vasm${VASM_VERSION}.tar.gz 
+  cd vasm
+  make CPU=m68k SYNTAX=mot
+  make CPU=m68k SYNTAX=std
+  make CPU=x86 SYNTAX=std
+  make CPU=ppc SYNTAX=std
+  make CPU=arm SYNTAX=std
+  cp vasmm68k_mot vobjdump vasmm68k_std vasmx86_std vasmppc_std vasmarm_std $HOME/bin
+fi
+
+# Add vlink linker
+if [ ! -f $HOME/bin/vlink ] ; then
+  cd $HOME/gnu
+  mkdir vlink
+  cd vlink
+  wget http://server.owl.de/~frank/tags/vlink${VLINK_VERSION}.tar.gz
+  tar -xvzf vlink${VLINK_VERSION}.tar.gz 
+  cd vlink
+  make
+  cp vlink $HOME/bin
+fi
 
