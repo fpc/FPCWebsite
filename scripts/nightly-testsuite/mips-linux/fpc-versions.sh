9a1a4edbe25211e6b81c2adc1b47a5b115b509bb
pierre <pierre@freepascal.org>
2018-12-12 15:59:22 +0000

    Add TRUNKDIRNAME and FIXESDIRNAME variables
    git-svn-id: @1289 -

--- scripts/nightly-testsuite/mips-linux/fpc-versions.sh
+++ scripts/nightly-testsuite/mips-linux/fpc-versions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export TRUNKDIR=~/pas/trunk
+export TRUNKDIRNAME=trunk
+export TRUNKDIR=~/pas/$TRUNKDIRNAME
 export RELEASEVERSION=3.0.4
 export TRUNKVERSION=3.3.1
-export FIXESDIR=~/pas/fixes_3_2
+export FIXESDIRNAME=fixes_3_2
+export FIXESDIR=~/pas/$FIXESDIRNAME
 export FIXESVERSION=3.2.0
