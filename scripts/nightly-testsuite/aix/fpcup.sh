893f65b707cb41e9408d3065350f7ddb207d3954
pierre <pierre@freepascal.org>
2019-01-10 16:42:09 +0000

    Call makesnapshot-aix.sh as a subshell, by exporting FIXES and FPC_BIN variables
    git-svn-id: @1317 -

--- scripts/nightly-testsuite/aix/fpcup.sh
+++ scripts/nightly-testsuite/aix/fpcup.sh
@@ -1,16 +1,17 @@
+#!/usr/bin/env bash
 env > $HOME/.env.txt
 date >> $HOME/.env.txt
 $HOME/bin/fpcfixesup.sh
 $HOME/bin/fpctrunkup.sh
 
-FIXES=0
-FPC_BIN=ppcppc
-. $HOME/bin/makesnapshot-aix.sh
-FPC_BIN=ppcppc64
-. $HOME/bin/makesnapshot-aix.sh
+export FIXES=0
+export FPC_BIN=ppcppc
+$HOME/bin/makesnapshot-aix.sh
+export FPC_BIN=ppcppc64
+$HOME/bin/makesnapshot-aix.sh
 
-FIXES=1
-FPC_BIN=ppcppc
-. $HOME/bin/makesnapshot-aix.sh
-FPC_BIN=ppcppc64
-. $HOME/bin/makesnapshot-aix.sh
+export FIXES=1
+export FPC_BIN=ppcppc
+$HOME/bin/makesnapshot-aix.sh
+export FPC_BIN=ppcppc64
+$HOME/bin/makesnapshot-aix.sh
