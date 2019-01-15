c88eae181b6e48a5eb8b34ba421678318710d8aa
pierre <pierre@freepascal.org>
2019-01-15 14:27:51 +0000

    Remove extra erroneous line
    git-svn-id: @1321 -

--- scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
+++ scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
@@ -24,7 +24,6 @@ $HOME/bin/check-all-rtl.sh
 
 if [ $enable_64bit_tests -eq 1 ] ; then
    $HOME/bin/test-cross-64.sh
-if [ "$DO_SNAPSHOTS" == "1" ] ; then
    if [ "$DO_SNAPSHOTS" == "1" ] ; then
      $HOME/bin/makesnapshottrunk64.sh
    fi
