4bcbfbe87668ad705363d35da471d05cc16010e2
pierre <pierre@freepascal.org>
2019-01-25 22:38:27 +0000

    * Reorder scripts, first generate snapshots and only after execute check-all-rtl.sh script.   + Also call generate-cross-fpux80.sh script for fixes branch
    git-svn-id: @1350 -

--- scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
+++ scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
@@ -14,27 +14,38 @@ today_sparc_linux_fixes=`ssh fpcftp "find ftp/snapshot/fixes/sparc-linux/ -name 
 today_sparc64_linux_fixes=`ssh fpcftp "find ftp/snapshot/fixes/sparc64-linux/ -name "fpc*.gz" -newermt $TODAY" 2> /dev/null `
 
 export FIXES=1
-. $HOME/bin/linux-fpccommonup.sh
-$HOME/bin/check-all-rtl.sh
+$HOME/bin/linux-fpccommonup.sh
 if [ "$DO_SNAPSHOTS" == "1" ] ; then
   if [ -z "$today_sparc_linux_fixes" ] ; then
     $HOME/bin/makesnapshotfixes.sh
   fi
 fi
 export FIXES=0
-. $HOME/bin/linux-fpccommonup.sh
+$HOME/bin/linux-fpccommonup.sh
 if [ "$DO_SNAPSHOTS" == "1" ] ; then
   if [ -z "$today_sparc_linux_trunk" ] ; then
     $HOME/bin/makesnapshottrunk.sh
   fi
 fi
+
+export FIXES=1
+$HOME/bin/check-all-rtl.sh
+export FIXES=0
 $HOME/bin/check-all-rtl.sh
 
 cd $HOME/pas/trunk/fpcsrc/compiler
 # regenerate cross-compilers for check-all-rtl.sh script
-make fullcycle fullinstall INSTALL_PREFIX=$HOME/pas/fpc-$TRUNKVERSION FPC=$HOME/pas/fpc-$TRUNKVERSION/bin/ppcsparc > $HOME/logs/trunk-fullinstall.log 2>&1
+make fullcycle fullinstallsymlink INSTALL_PREFIX=$HOME/pas/fpc-$TRUNKVERSION FPC=$HOME/pas/fpc-$TRUNKVERSION/bin/ppcsparc > $HOME/logs/trunk-fullinstall.log 2>&1
 # Update ppc386, ppcx64 and ppc8086 with softfpu extended emulation
+export FIXES=0
 $HOME/bin/generate-cross-sfpux80.sh > $HOME/logs/trunk-generate-sfpux80.log 2>&1
+
+cd $HOME/pas/fixes/fpcsrc/compiler
+# regenerate cross-compilers for check-all-rtl.sh script
+make fullcycle fullinstallsymlink INSTALL_PREFIX=$HOME/pas/fpc-$FIXESVERSION FPC=$HOME/pas/fpc-$FIXESVERSION/bin/ppcsparc > $HOME/logs/fixes-fullinstall.log 2>&1
+# Update ppc386, ppcx64 and ppc8086 with softfpu extended emulation
+export FIXES=1
+$HOME/bin/generate-cross-sfpux80.sh > $HOME/logs/fixes-generate-sfpux80.log 2>&1
 
 if [ $enable_64bit_tests -eq 1 ] ; then
   $HOME/bin/test-cross-64.sh
