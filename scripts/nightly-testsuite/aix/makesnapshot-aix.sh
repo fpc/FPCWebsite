3604d59b12f6cd8275abecab538161bb303dbc81
pierre <pierre@freepascal.org>
2019-01-28 23:28:09 +0000

    Small improvements
    git-svn-id: @1361 -

--- scripts/nightly-testsuite/aix/makesnapshot-aix.sh
+++ scripts/nightly-testsuite/aix/makesnapshot-aix.sh
@@ -6,43 +6,71 @@ if [ -z "$FIXES" ] ; then
   FIXES=0
 fi
 
-if [ $FIXES -eq 0 ] ; then
+if [ $FIXES -eq 1 ] ; then
+  echo "Starting fixes snapshot"
   FPC_VER=$FIXESVERSION
   SVNDIR=$FIXESDIR
   FTP_SNAPSHOT_DIR=fixes
 else
+  echo "Starting trunk snapshot"
   FPC_VER=$TRUNKVERSION
   SVNDIR=$TRUNKDIR
   FTP_SNAPSHOT_DIR=trunk
 fi
 
-if [ -z "$FPC_BIN"  ; then
+if [ -z "$FPC_BIN" ] ; then
   FPC_BIN=ppcppc
 fi
 
-FPC_CPU=`$FPC_BIN -iTP`
-FPC_OS=`$FPC_BIN -iTO`
-FPC_FULLVER=`$FPC_BIN -iW`
+FULL_PATH_FPC=`which $FPC_BIN 2> /dev/null`
+
+if [ -z "$FULL_PATH_FPC" ] ; then
+  export PATH=$HOME/pas/fpc-$RELEASEVERSION/bin:$PATH
+fi
+
+export FPC=~/pas/fpc-${FPC_VER}/bin/${FPC_BIN}
+
+FPC_CPU=`$FPC -iTP`
+FPC_OS=`$FPC -iTO`
+FPC_FULLVER=`$FPC -iW`
 
 FPC_OS=aix
 FPC_CPUOS=${FPC_CPU}-${FPC_OS}
 
-export FPC=~/pas/fpc-${FPC_VER}/bin/${FPC_BIN}
-
 if [ "${PATH/${FPC_VER}//}" = "${PATH}" ] ; then
   echo "Adding ${FPC_VER} binary directory"
-  export PATH=${PATH}:$HOME/pas/fpc-${FPC_VER}/bin
+  export PATH=$HOME/pas/fpc-${FPC_VER}/bin:$PATH
 fi
 
-MAKE=make
+if [ -z "$MAKE" ]; then
+  MAKE=` which gmake 2> /dev/null `
+  if [ -z "$MAKE" ] ; then
+    MAKE=make
+  fi
+fi
+
+
 MAKE_OPTIONS="distclean singlezipinstall SNAPSHOT=1"
 date=`date +%Y-%m-%d`
 
 
 cd $SVNDIR
 
-cat > README <<EOF
-This snapshot was generated ${date} using:
+
+TAR=`ls -1tr ./fpc-${FPC_VER}*.${FPC_CPUOS}.tar.gz | head -1`
+
+if [ -f "${TAR}" ] ; then
+  mv -f  ${TAR} ${TAR}.old
+fi
+
+${MAKE} ${MAKE_OPTIONS} > $HOME/logs/makesnapshot-${FTP_SNAPSHOT_DIR}-${FPC_CPU}-${date}.txt 2>&1
+res=$?
+
+TAR=`ls -1tr ./fpc-${FPC_VER}*.${FPC_CPUOS}.tar.gz | head -1`
+
+if [ -f ${TAR} ]; then
+cat > readme <<EOF
+This $TAR snapshot was generated ${date} using:
 ${MAKE} ${MAKE_OPTIONS}
 started using ${FPC}
 ${FPC_BIN} -iVDW output is: `${FPC} -iVDW`
@@ -59,19 +87,7 @@ Enjoy,
 Pierre Muller
 EOF
 
-
-TAR=`ls -1tr ./fpc-${FPC_VER}*.${FPC_CPUOS}.tar.gz | head -1`
-
-if [ -f "${TAR}" ] ; then
-  mv -f  ${TAR} ${TAR}.old
-fi
-
-${MAKE} ${MAKE_OPTIONS} | tee makesnapshot-${date}.txt
-
-TAR=`ls -1tr ./fpc-${FPC_VER}*.${FPC_CPUOS}.tar.gz | head -1`
-
-if [ -f ${TAR} ]; then
-  scp ${TAR} README fpcftp:ftp/snapshot/$FTP_SNAPSHOT_DIR/${FPC_CPUOS}
+  scp ${TAR} readme fpcftp:ftp/snapshot/$FTP_SNAPSHOT_DIR/${FPC_CPUOS}
 else
   echo Failed to created ${TAR} file
 fi
