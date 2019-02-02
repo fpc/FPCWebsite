56d8ef33fa4de474d4d911990f9a56c1e68bc500
pierre <pierre@freepascal.org>
2019-02-02 23:17:27 +0000

    Adapt script to the four CPU variants: i386/x86_64/sparc and sparc64
    git-svn-id: @1379 -

--- scripts/nightly-testsuite/solaris/makesnapshots.sh
+++ scripts/nightly-testsuite/solaris/makesnapshots.sh
@@ -9,14 +9,16 @@ if [ "$CPU" == "sparc" ] ; then
     sleep 60
   done
 
-  echo "Generating snapshot for sparc" > $LOCKFILE
+  FIXES=
+  echo "Generating trunk snapshot for sparc" > $LOCKFILE
   CHECKOUTDIR=$HOME/pas/trunk
   LOGFILE=~/logs/makesnapshot-trunk-sparc.log
   FTPDIR=fpcftp:ftp/snapshot/trunk/sparc-solaris
   STARTPP=ppcsparc
   . $HOME/bin/makesnapshot.sh > $LOGFILE 2>&1
   if [ ! -z "$SPARC64_SNAPSHOT" ] ; then
-    echo "Generating snapshot for sparc64" > $LOCKFILE
+    echo "Generating trunk snapshot for sparc64" > $LOCKFILE
+    CHECKOUTDIR=$HOME/pas/sparc64-solaris-trunk
     LOGFILE=~/logs/makesnapshot-trunk-sparc64.log
     FTPDIR=fpcftp:ftp/snapshot/trunk/sparc64-solaris
     STARTPP=ppcsparc64
@@ -28,8 +30,8 @@ if [ "$CPU" == "sparc" ] ; then
   while [ -f $LOCKFILE ] ; do
     sleep 60
   done
-
-  echo "Generating snapshot for sparc" > $LOCKFILE
+  FIXES=1
+  echo "Generating fixes snapshot for sparc" > $LOCKFILE
   CHECKOUTDIR=$HOME/pas/fixes
   LOGFILE=~/logs/makesnapshot-fixes-sparc.log
   FTPDIR=fpcftp:ftp/snapshot/fixes/sparc-solaris
@@ -44,13 +46,14 @@ if [ "$CPU" == "i386" ] ; then
     sleep 60
   done
 
-  echo "Generating snapshot for i386" > $LOCKFILE
+  FIXES=
+  echo "Generating trunk snapshot for i386" > $LOCKFILE
   CHECKOUTDIR=$HOME/pas/trunk
   LOGFILE=~/logs/makesnapshot-trunk-i386.log
   FTPDIR=fpcftp:ftp/snapshot/trunk/i386-solaris
   STARTPP=ppc386
   . $HOME/bin/makesnapshot.sh > $LOGFILE 2>&1
-  echo "Generating snapshot for x86_64" > $LOCKFILE
+  echo "Generating trunk snapshot for x86_64" > $LOCKFILE
   LOGFILE=~/logs/makesnapshot-trunk-x86_64.log
   FTPDIR=fpcftp:ftp/snapshot/trunk/x86_64-solaris
   STARTPP=ppcx64
@@ -62,12 +65,14 @@ if [ "$CPU" == "i386" ] ; then
     sleep 60
   done
 
-  echo "Generating snapshot for i386" > $LOCKFILE
+  FIXES=1
+  echo "Generating fixes snapshot for i386" > $LOCKFILE
   CHECKOUTDIR=$HOME/pas/fixes
   LOGFILE=~/logs/makesnapshot-fixes-i386.log
   FTPDIR=fpcftp:ftp/snapshot/fixes/i386-solaris
   STARTPP=ppc386
   . $HOME/bin/makesnapshot.sh > $LOGFILE 2>&1
+  echo "Generating fixes snapshot for x86_64" > $LOCKFILE
   LOGFILE=~/logs/makesnapshot-fixes-x86_64.log
   FTPDIR=fpcftp:ftp/snapshot/fixes/x86_64-solaris
   STARTPP=ppcx64
