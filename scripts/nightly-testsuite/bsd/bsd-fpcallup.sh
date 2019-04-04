5f5c7efa5bdce5b1d7c50f35a63070443ddb40ab
pierre <pierre@freepascal.org>
2019-04-04 06:19:14 +0000

    Handle OpenBSD specificity: x86_64 and i386 binaries are incompatible
    git-svn-id: @1415 -

--- scripts/nightly-testsuite/bsd/bsd-fpcallup.sh
+++ scripts/nightly-testsuite/bsd/bsd-fpcallup.sh
@@ -3,17 +3,48 @@
 # Always generate debug information
 export NEEDED_OPT=-g
 
+OS=`uname -s | tr '[:upper:]' '[:lower:]' `
+CPU=`uname -m | tr '[:upper:]' '[:lower:]' `
+if [ "$CPU" == "amd64" ] ; then
+  CPU=x86_64
+fi
+if [ "$CPU" == "arm64" ] ; then
+  CPU=aarch64
+fi
+
+
+run_32=1
+run_64=1
+
+if [ "$OS" == "openbsd" ] ; then
+  if [ "$CPU" == "i386" ] ; then
+    run_64=0
+  fi
+  if [ "$CPU" == "x86_64" ] ; then
+    run_32=0
+  fi
+fi
+
 export FIXES=0
-export FPCBIN=ppcx64
-$HOME/bin/bsd-fpccommonup.sh
-export FPCBIN=ppc386
-$HOME/bin/bsd-fpccommonup.sh
+if [ $run_64 -eq 1 ] ; then
+  export FPCBIN=ppcx64
+  $HOME/bin/bsd-fpccommonup.sh
+fi
+
+if [ $run_32 -eq 1 ] ; then
+  export FPCBIN=ppc386
+  $HOME/bin/bsd-fpccommonup.sh
+fi
 
 export FIXES=1
-export FPCBIN=ppcx64
-$HOME/bin/bsd-fpccommonup.sh
-export FPCBIN=ppc386
-$HOME/bin/bsd-fpccommonup.sh
+if [ $run_64 -eq 1 ] ; then
+  export FPCBIN=ppcx64
+  $HOME/bin/bsd-fpccommonup.sh
+fi
+if [ $run_32 -eq 1 ] ; then
+  export FPCBIN=ppc386
+  $HOME/bin/bsd-fpccommonup.sh
+fi
 
 $HOME/bin/makesnapshottrunk.sh
 $HOME/bin/makesnapshotfixes.sh
