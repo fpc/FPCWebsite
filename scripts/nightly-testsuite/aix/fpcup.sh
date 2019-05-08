130d2f64bb0f2e14c81234c4469052c7be2537dc
pierre <pierre@freepascal.org>
2019-05-08 19:34:43 +0000

    Check for gmake and add $HOME/bin to front of PATH variable
    git-svn-id: @1424 -

--- scripts/nightly-testsuite/aix/fpcup.sh
+++ scripts/nightly-testsuite/aix/fpcup.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 env > $HOME/.env.txt
 date >> $HOME/.env.txt
+
+if [ -z "$MAKE" ]; then
+  MAKE=` which gmake 2> /dev/null `
+  if [ -z "$MAKE" ] ; then
+    MAKE=make
+  fi
+fi
+export MAKE
+
 $HOME/bin/fpcfixesup.sh
 $HOME/bin/fpctrunkup.sh
+
+# Ensure correct GNU diffutils cmp is found
+if [ -d $HOME/bin ] ; then
+  export PATH=$HOME/bin:$PATH
+fi
 
 export FIXES=0
 export FPC_BIN=ppcppc
