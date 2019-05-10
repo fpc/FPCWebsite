9a8be83f89257e920ae7ebffdbd9c2ab489b1195
pierre <pierre@freepascal.org>
2019-05-10 13:05:16 +0000

    Set ARM_ABI to gnueabihf for 32bit arm on aarch64 machine
    git-svn-id: @1426 -

--- scripts/nightly-testsuite/aarch64-linux/linux32-fpctrunkup.sh
+++ scripts/nightly-testsuite/aarch64-linux/linux32-fpctrunkup.sh
@@ -2,7 +2,8 @@
 export FPCBIN=ppcarm
 export LOGSUF=-32
 if [ -z "$REQUIRED_ARM_OPT" ] ; then
-  export REQUIRED_ARM_OPT=" -dFPC_ARMHF -Cparmv7a -Fl$HOME/sys-root/arm-linux-gnueabihf/lib"
+  export ARM_ABI=gnueabihf
+  export REQUIRED_ARM_OPT=" -dFPC_ARMHF -Cparmv7a -Fl/usr/arm-linux-$ARM_ABI/lib"
 fi
 $HOME/bin/linux-fpctrunkup.sh
 
