4356adfc39a1dc19f02791680413f719c1b2dea8
pierre <pierre@freepascal.org>
2019-05-16 11:13:37 +0000

    + gen_ppu_log: new function to generate a log using ppudump   + gen_ppu_diff: new function to generate a diff file from two logs     generated by new gen_ppu_log function.   + skip_tests: new variable, to enable skipping of testsuite part.   + Check system, objpas and sysutils units using gen_ppu_log and gen_ppu_diff
    git-svn-id: @1435 -

--- scripts/nightly-testsuite/i386-linux/linux-fpccommonup.sh
+++ scripts/nightly-testsuite/i386-linux/linux-fpccommonup.sh
@@ -3,6 +3,9 @@
 . $HOME/bin/fpc-versions.sh
 # Limit resources (64mb data, 8mb stack, 40 minutes)
 
+if [ -z "$skip_tests" ] ; then
+  skip_tests=0
+fi
 
 export MAKE=make
 if [ "${HOSTNAME}" == "vadmin" ]; then
@@ -34,9 +37,47 @@ fi
 
 cd ~/pas/${SVNDIR}
 
-export report=`pwd`/report.txt
-export makelog=`pwd`/make.txt
-export testslog=`pwd`/tests.txt
+logdir=~/logs/$SVNDIR
+if [ ! -d $logdir ] ; then
+  mkdir -p $logdir
+fi
+
+export report=$logdir/report.txt
+export makelog=$logdir/make.txt
+export testslog=$logdir/tests.txt
+
+function gen_ppu_log ()
+{
+  dir="$1"
+  ppufilename="$2"
+  ppusuff="$3"
+  ppu="$dir/$ppufilename"
+  logfile="$logdir/${ppufilename}${ppusuff}"
+  if [ -f /home/${USER}/pas/fpc-${Build_version}/bin/ppudump ] ; then
+    echo "/home/${USER}/pas/fpc-${Build_version}/bin/ppudump \"$ppu\" > \"$logfile\"" >> $makelog
+    /home/${USER}/pas/fpc-${Build_version}/bin/ppudump "$ppu" > "$logfile"
+    grep -E "(^Analysing|Checksum)" "$logfile" > "$logfile"-short 2>&1
+  fi
+}
+
+function gen_ppu_diff ()
+{
+  ppufilename="$1"
+  ppusuff1="$2"
+  ppusuff2="$3"
+  ppu1="$logdir/$ppufilename$ppusuff1"
+  ppu2="$logdir/$ppufilename$ppusuff2"
+  difffile="$logdir/${ppufilename}.ppudiffs"
+  echo "diff -c \"${ppu1}\" \"${ppu2}\" > \"${difffile}\"" >> $makelog
+  diff -c "${ppu1}" "${ppu2}" > "${difffile}"
+  diffres=$?
+  if [ $diffres -ne 0 ] ; then
+    echo "ppudump output changed" >> $report
+    cat "${difffile}" >> $report
+    echo "Cleaning packages to be sure" >> $report
+    make -C packages distclean FPC=$NEW_PPC_BIN 1>> ${makelog} 2>&1
+  fi
+}
 
 echo "Starting $0" > $report
 START_PPC_BIN=`which $FPCBIN 2> /dev/null`
@@ -81,15 +122,16 @@ NEW_CPU_TARGET=`$NEW_PPC_BIN -iTP`
 echo "New $FPCBIN version is ${Build_version} ${Build_date} ${NEW_CPU_TARGET}-${NEW_OS_TARGET} " >> $report
 
 # Register system.ppu state
-if [ -f /home/${USER}/pas/fpc-${Build_version}/bin/ppudump ] ; then
-  /home/${USER}/pas/fpc-${Build_version}/bin/ppudump rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu | grep -E "(^Analysing|Checksum)" > rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-log1 2>&1
-fi
-# Update all cross-compilers (without DEBUG set)
-echo "make -C compiler cycle install fullcycle fullinstall INSTALL_PREFIX=~/pas/fpc-${Build_version}" >> $report
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} system.ppu -log1
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} objpas.ppu -log1
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} sysutils.ppu -log1
+
+# Update all cross-compilers (with DEBUG set)
+echo "make -C compiler cycle install fullcycle fullinstall DEBUG=1 INSTALL_PREFIX=~/pas/fpc-${Build_version}" >> $report
 NEW_PPC_SAFE=`pwd`/compiler/startppc
 cp $NEW_PPC_BIN $NEW_PPC_SAFE
 
-make -C compiler cycle install fullcycle fullinstall INSTALL_PREFIX=~/pas/fpc-${Build_version} FPC=$NEW_PPC_SAFE 1>> ${makelog} 2>&1
+make -C compiler cycle install fullcycle fullinstall RELEASE=1 DEBUG=1 INSTALL_PREFIX=~/pas/fpc-${Build_version} FPC=$NEW_PPC_SAFE 1>> ${makelog} 2>&1
 makeres=$?
 if [ $makeres -ne 0 ] ; then
   echo "${MAKE} distclean all failed result=${makeres}" >> $report
@@ -98,18 +140,12 @@ else
   echo "Ending make distclean all; result=${makeres}" >> $report
 fi
 # Check if system.ppu changed
-if [ -f /home/${USER}/pas/fpc-${Build_version}/bin/ppudump ] ; then
-  /home/${USER}/pas/fpc-${Build_version}/bin/ppudump rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu | grep -E "(^Analysing|Checksum)"  > rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-log2 2>&1
-  diff -c  rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-log1 rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-log2 > rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-logdiffs
-  diffres=$?
-  if [ $diffres -ne 0 ] ; then
-    echo "ppudump output changed" >> $report
-    cat rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET}/system.ppu-logdiffs >> $report
-    echo "Cleaning packages to be sure" >> $report
-    make -C packages distclean FPC=$NEW_PPC_BIN 1>> ${makelog} 2>&1
-  fi
-fi
-
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} system.ppu -log2
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} objpas.ppu -log2
+gen_ppu_log rtl/units/${NEW_CPU_TARGET}-${NEW_OS_TARGET} sysutils.ppu -log2
+gen_ppu_diff system.ppu -log1 -log2
+gen_ppu_diff objpas.ppu -log1 -log2
+gen_ppu_diff sysutils.ppu -log1 -log2
 echo "Starting make install" >> $report
 echo "`$DATE`" >> $report
 ${MAKE} DEBUG=1 install INSTALL_PREFIX=~/pas/fpc-${Build_version} FPC=$NEW_PPC_SAFE 1>> ${makelog} 2>&1
@@ -137,48 +173,50 @@ NEW_OS_TARGET=`$NEWFPC -iTO`
 NEW_CPU_TARGET=`$NEWFPC -iTP`
 NEW_FULL_TARGET=${NEW_CPU_TARGET}-${NEW_OS_TARGET}
 
-cd tests
-# Limit resources (64mb data, 8mb stack, 4 minutes)
 
-ulimit -d 65536 -s 8192 -t 240
-
-echo "Starting make distclean fulldb" >> $report
-echo "`$DATE`" >> $report
-${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
+if [ $skip_tests -eq 0 ] ; then
+  cd tests
+  # Limit resources (64mb data, 8mb stack, 4 minutes)
+  
+  ulimit -d 65536 -s 8192 -t 240
+  
+  echo "Starting make distclean fulldb" >> $report
+  echo "`$DATE`" >> $report
+  ${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
   TEST_OPT="" TEST_FPC=${NEWFPC} FPC=${NEWFPC} \
   DB_SSH_EXTRA=" -i ~/.ssh/freepascal" 1> $testslog 2>&1
-testsres=$?
-echo "Ending make distclean fulldb; result=${testsres}" >> $report
-echo "`$DATE`" >> $report
-
-tail -43 $testslog >> $report
-
-TEST_OPT="-Cg"
-export testslog=`pwd`/tests-Cg.txt
-echo "Starting make clean fulldb with TEST_OPT=${TEST_OPT}" >> $report
-echo "`$DATE`" >> $report
-${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
+  testsres=$?
+  echo "Ending make distclean fulldb; result=${testsres}" >> $report
+  echo "`$DATE`" >> $report
+  
+  tail -43 $testslog >> $report
+  
+  TEST_OPT="-Cg"
+  export testslog=$logdir/tests-Cg.txt
+  echo "Starting make clean fulldb with TEST_OPT=${TEST_OPT}" >> $report
+  echo "`$DATE`" >> $report
+  ${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
   TEST_OPT="${TEST_OPT}" TEST_FPC=${NEWFPC} FPC=${NEWFPC} \
   DB_SSH_EXTRA=" -i ~/.ssh/freepascal" 1> $testslog 2>&1
-testsres=$?
-echo "Ending make distclean fulldb with TEST_OPT=${TEST_OPT}; result=${testsres}" >> $report
-echo "`$DATE`" >> $report
-
-tail -43 $testslog >> $report
-
-TEST_OPT="-O4 -gwl"
-export testslog=`pwd`/tests-O4-gwl.txt
-echo "Starting make clean fulldb with TEST_OPT=${TEST_OPT}" >> $report
-echo "`$DATE`" >> $report
-${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
+  testsres=$?
+  echo "Ending make distclean fulldb with TEST_OPT=${TEST_OPT}; result=${testsres}" >> $report
+  echo "`$DATE`" >> $report
+  
+  tail -43 $testslog >> $report
+  
+  TEST_OPT="-O4 -gwl"
+  export testslog=$logdir/tests-O4-gwl.txt
+  echo "Starting make clean fulldb with TEST_OPT=${TEST_OPT}" >> $report
+  echo "`$DATE`" >> $report
+  ${MAKE} distclean fulldb TEST_USER=pierre TEST_HOSTNAME=${HOST_PC} \
   TEST_OPT="${TEST_OPT}" TEST_FPC=${NEWFPC} FPC=${NEWFPC} \
   DB_SSH_EXTRA=" -i ~/.ssh/freepascal" 1> $testslog 2>&1
-testsres=$?
-echo "Ending make distclean fulldb with TEST_OPT=${TEST_OPT}; result=${testsres}" >> $report
-echo "`$DATE`" >> $report
-
-tail -43 $testslog >> $report
-
+  testsres=$?
+  echo "Ending make distclean fulldb with TEST_OPT=${TEST_OPT}; result=${testsres}" >> $report
+  echo "`$DATE`" >> $report
+  
+  tail -43 $testslog >> $report
+fi
 
 mutt -x -s "Free Pascal results for ${NEW_FULL_TARGET} on ${HOST_PC}, ${Build_version} ${Build_date}" \
      -i $report -- pierre@freepascal.org < /dev/null | tee  ${report}.log
