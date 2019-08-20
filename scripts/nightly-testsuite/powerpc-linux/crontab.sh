c6ef294bcc408d5c3c048982e4b5785a9a338517
pierre <pierre@freepascal.org>
2019-08-20 13:45:39 +0000

    Handle ppc64le systems
    git-svn-id: @1486 -

--- scripts/nightly-testsuite/powerpc-linux/crontab.sh
+++ scripts/nightly-testsuite/powerpc-linux/crontab.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 . ~/bin/fpc-versions.sh
 
+machine=`uname -m`
+
+if [ "$machine" = "ppc64le" ] ; then
+  gen_64bit=1
+  gen_32bit=0
+elif [ "$machine" = "ppc64" ] ; then
+  gen_64bit=1
+  gen_32bit=1
+elif [ "$machine" = "ppc" ] ; then
+  gen_64bit=0
+  gen_32bit=1
+else
+  gen_64bit=0
+  gen_32bit=0
+  echo "Warning: unrecognized machine $machine"
+fi
+
 export CURVER=$TRUNKVERSION
 export RELEASEVER=$RELEASEVERSION
 export FTP_SNAPSHOT_DIR=$TRUNKDIRNAME
 export FPCSVNDIR=$TRUNKDIR
 
-. ~/bin/linux64-fpctrunkup.sh
-. ~/bin/linux32-fpctrunkup.sh
-
-. ~/bin/makesnapshot-new-powerpc.sh
-. ~/bin/makesnapshot-new-powerpc64.sh
+if [ $gen_64bit -eq 1 ] ; then
+  . ~/bin/linux64-fpctrunkup.sh
+  . ~/bin/makesnapshot-new-powerpc64.sh
+fi
+if [ $gen_32bit -eq 1 ] ; then
+  . ~/bin/linux32-fpctrunkup.sh
+  . ~/bin/makesnapshot-new-powerpc.sh
+fi 
 
 export CURVER=$FIXESVERSION
 export RELEASEVER=$RELEASEVERSION
 export FTP_SNAPSHOT_DIR=$FIXESDIRNAME
 export FPCSVNDIR=$FIXESDIR
 
-. ~/bin/linux64-fpcfixesup.sh
-. ~/bin/linux32-fpcfixesup.sh
+if [ $gen_64bit -eq 1 ] ; then
+ . ~/bin/linux64-fpcfixesup.sh
+ . ~/bin/makesnapshot-new-powerpc64.sh
+fi
 
-. ~/bin/makesnapshot-new-powerpc.sh
-. ~/bin/makesnapshot-new-powerpc64.sh
+if [ $gen_32bit -eq 1 ] ; then
+  . ~/bin/linux32-fpcfixesup.sh
+  . ~/bin/makesnapshot-new-powerpc.sh
+fi
+
