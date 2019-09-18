8f2af151b5df2926eba4a6a73961facbd464b398
pierre <pierre@freepascal.org>
2019-09-18 21:33:55 +0000

    Update and improve script
    git-svn-id: @1502 -

--- scripts/cross-compile/install-qemu.sh
+++ scripts/cross-compile/install-qemu.sh
@@ -2,14 +2,27 @@
 
 QEMU_VERSION="$1"
 if [ -z "$QEMU_VERSION" ] ; then
-  QEMU_VERSION=3.1.0
+  QEMU_VERSION=4.1.0
 fi
-echo "Downloading qem version $QEMU_VERSION"
-wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
-echo "Unpacking qem version $QEMU_VERSION"
+if [ ! -f qemu-$QEMU_VERSION.tar.xz ] ; then
+  echo "Downloading qemu version $QEMU_VERSION"
+  wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
+fi
+if [ -d qemu-$QEMU_VERSION ] ; then
+  rm -Rf qemu-$QEMU_VERSION
+fi
+
+echo "Unpacking qemu version $QEMU_VERSION"
 tar xvJf qemu-$QEMU_VERSION.tar.xz
 cd qemu-$QEMU_VERSION
-echo "Configuring qem version $QEMU_VERSION"
-./configure --prefix=$HOME/sys-root
-echo "Making qem version $QEMU_VERSION"
+echo "Configuring qemu version $QEMU_VERSION"
+export CFLAGS="-gdwarf-4 -I$HOME/gnu/include -L$HOMR/gnu/lib" 
+./configure --prefix=$HOME/sys-root --enable-curses --extra-cflags="$CFLAGS"
+echo "Making qemu version $QEMU_VERSION"
 make
+res=$?
+
+if [ $res -eq 0 ] ; then
+  echo "Make finished OK, you can install using \"make install\""
+fi
+
