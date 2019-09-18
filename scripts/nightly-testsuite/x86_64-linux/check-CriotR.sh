d7df72532c9cfdd4f4f76a8df453a2626db25454
pierre <pierre@freepascal.org>
2019-09-18 06:15:17 +0000

    * check-CriotR.sh: Use BINUTILSPREFIX for i386-linux if i386-linux-ld is found.     Add MAKE_EXTRA and TEST_BINUTILSPREFIX variables.   * Do not install i386-lnux as it changes the default compiler target.
    git-svn-id: @1501 -

--- scripts/nightly-testsuite/x86_64-linux/check-CriotR.sh
+++ scripts/nightly-testsuite/x86_64-linux/check-CriotR.sh
@@ -11,6 +11,13 @@ libgcc32=`gcc -m32 -print-libgcc-file-name`
 if [ -f "$libgcc32" ] ; then
   COMMON_OPT32="-Fl`dirname $libgcc32`"
 fi
+
+i386_gnu_ld=`which i386-linux-ld`
+if [ -f "$i386_gnu_ld" ] ; then
+  MAKE_EXTRA32="BINUTILSPREFIX=i386-linux-"
+  TEST_BINUTILSPREFIX32=i386-linux-
+fi
+
 entrydir=`pwd`
 
 function run_tests ()
@@ -29,18 +36,22 @@ function run_tests ()
   TEST_CPU_TARGET=`$TEST_FULL_FPC -iTP`
   if [ "$TEST_CPU_TARGET" == "x86_64" ] ; then
     TEST_OPT="$TEST_OPT $COMMON_OPT64"
+    MAKE_EXTRA="$MAKE_EXTRA64"
+    TEST_BINUTILSPREFIX="$TEST_BINUTILSPREFIX64"
   fi
   if [ "$TEST_CPU_TARGET" == "i386" ] ; then
     TEST_OPT="$TEST_OPT $COMMON_OPT32"
+    MAKE_EXTRA="$MAKE_EXTRA32"
+    TEST_BINUTILSPREFIX="$TEST_BINUTILSPREFIX32"
   fi
   TEST_OS_TARGET=`$TEST_FULL_FPC -iTO`
   cd $entrydir
   echo "Running tests for $TEST_CPU_TARGET-$TEST_OS_TARGET using $TEST_FULL_FPC"
   make -C .. distclean TEST_FPC=$TEST_FULL_FPC FPC=fpc > fpcsrc-distclean-$TEST_FPC.log 2>&1
   make distclean TEST_FPC=$TEST_FULL_FPC FPC=fpc > tests-distclean-$TEST_FPC.log 2>&1
-  make -C ../rtl clean all FPC=fpc OPT="$OPT" > rtl-fpcsrc-$TEST_FPC.log 2>&1
-  make -C ../packages clean fpmake  FPC=fpc OPT="$OPT" > packages-fpmake-$TEST_FPC.log 2>&1
-  make -j 5 full TEST_FPC=$TEST_FULL_FPC TEST_OPT="$TEST_OPT" FPC=fpc OPT="$OPT" > full-$TEST_FPC.log 2>&1
+  make -C ../rtl clean all FPC=fpc OPT="$OPT" $MAKE_EXTRA > rtl-fpcsrc-$TEST_FPC.log 2>&1
+  make -C ../packages clean fpmake  FPC=fpc OPT="$OPT" $MAKE_EXTRA > packages-fpmake-$TEST_FPC.log 2>&1
+  make -j 5 full TEST_FPC=$TEST_FULL_FPC TEST_OPT="$TEST_OPT" TEST_BINUTILSPREFIX=$TEST_BINUTILSPREFIX $MAKE_EXTRA FPC=fpc OPT="$OPT" > full-$TEST_FPC.log 2>&1
   if [ -d output/$TEST_CPU_TARGET-$TEST_OS_TARGET ] ; then
     cd output/$TEST_CPU_TARGET-$TEST_OS_TARGET
     mv faillist ../../faillist-$TEST_FPC
@@ -68,21 +79,21 @@ function checkres ()
     FPC_VERSION=`ppcx64 -iV`
     export INSTALL_PREFIX=$HOME/pas/fpc-$FPC_VERSION
   fi
-  make_line='make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" CPU_TARGET=x86_64'
+  make_line="make -C ../compiler distclean cycle OPT=\"-n $COMMON_OPT $COMMON_OPT64\" CPU_TARGET=x86_64"
   echo "Starting $make_line"
-  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" CPU_TARGET=x86_64 > ppcx64-cycle.log 2>&1
+  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT $COMMON_OPT64" CPU_TARGET=x86_64 > ppcx64-cycle.log 2>&1
   res=$?
   checkres $make_line
 
-  make_line='make -C ../compiler installsymlink OPT="-n $COMMON_OPT" FPC=`pwd`/../compiler/ppcx64'
+  make_line="make -C ../compiler installsymlink OPT=\"-n $COMMON_OPT $COMMON_OPT64\" FPC=`pwd`/../compiler/ppcx64"
   echo "Starting $make_line"
-  make -C ../compiler installsymlink OPT="-n $COMMON_OPT" FPC=`pwd`/../compiler/ppcx64 > ppcx64-installsymlink.log 2>&1
+  make -C ../compiler installsymlink OPT="-n $COMMON_OPT $COMMON_OPT64" FPC=`pwd`/../compiler/ppcx64 > ppcx64-installsymlink.log 2>&1
   res=$?
   checkres $make_line
 
-  make_line='make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" LOCALOPT="$COMMON_OPT -CriotR" FPC=ppcx64'
+  make_line="make -C ../compiler distclean cycle OPT=\"-n $COMMON_OPT $COMMON_OPT64\" LOCALOPT=\"$COMMON_OPT -CriotR\" FPC=ppcx64"
   echo "Starting $make_line"
-  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" LOCALOPT="$COMMON_OPT -CriotR" FPC=ppcx64 > ppcx64-cycle-CriotR.log 2>&1
+  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT $COMMON_OPT64" LOCALOPT="$COMMON_OPT -CriotR" FPC=ppcx64 > ppcx64-cycle-CriotR.log 2>&1
   res=$?
   checkres $make_line
 
@@ -92,21 +103,20 @@ function checkres ()
   run_tests ppcx64
   run_tests ppcx64-CriotR
 
-  make_line='make -C ../compiler distclean cycle OPT="$COMMON_OPT" CPU_TARGET=i386'
+  make_line="make -C ../compiler distclean cycle OPT=\"$COMMON_OPT $COMMON_OPT32\" $MAKE_EXTRA32 CPU_TARGET=i386"
   echo "Starting $make_line"
-  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" CPU_TARGET=i386 > ppc386-cycle.log 2>&1
+  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT $COMMON_OPT32" $MAKE_EXTRA32 CPU_TARGET=i386 > ppc386-cycle.log 2>&1
   res=$?
   checkres $make_line
 
-  make_line='make -C ../compiler installsymlink OPT="$COMMON_OPT"  FPC=`pwd`/../compiler/ppc386'
-  echo "Starting $make_line"
-  make -C ../compiler installsymlink OPT="-n $COMMON_OPT"  FPC=`pwd`/../compiler/ppc386 > ppc386-installsymlink.log 2>&1
+  cp_line='cp ../compiler/ppc386 $INSTALL_PREFIX/bin/ppc386'
+  cp ../compiler/ppc386 $INSTALL_PREFIX/bin/ppc386
   res=$?
-  checkres $make_line
+  checkres $cp_line
 
-  make_line='make -C ../compiler distclean cycle OPT="$COMMON_OPT" LOCALOPT="$COMMON_OPT -CriotR" FPC=ppc386'
+  make_line='make -C ../compiler distclean cycle OPT="$COMMON_OPT $COMMON_OPT32" $MAKE_EXTRA32 LOCALOPT="$COMMON_OPT -CriotR" FPC=ppc386'
   echo "Starting $make_line"
-  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT" LOCALOPT="$COMMON_OPT -CriotR" FPC=ppc386 > ppc386-cycle-CriotR.log 2>&1
+  make -C ../compiler distclean cycle OPT="-n $COMMON_OPT $COMMON_OPT32" $MAKE_EXTRA32 LOCALOPT="$COMMON_OPT -CriotR" FPC=ppc386 > ppc386-cycle-CriotR.log 2>&1
   res=$?
   checkres $make_line
 
