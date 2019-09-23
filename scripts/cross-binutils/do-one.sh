44c0743da7ba9830271f5a99ec801c06698841b6
pierre <pierre@freepascal.org>
2019-09-23 14:32:35 +0000

    Use -z test to check if copy_only is set to a non-empty value
    git-svn-id: @1512 -

--- scripts/cross-binutils/do-one.sh
+++ scripts/cross-binutils/do-one.sh
@@ -226,7 +226,7 @@ function copytofpcbin ()
   fi
 
   cd ${prefix}
-  if [ "$copy_only" == "" ]; then
+  if [ -z "$copy_only" ]; then
     export "$LOCAL_CFLAGS"
     export LDFLAGS="${LDFLAGS}"
     ../../${binutilsdir}/configure $config_option --target=$target --with-sysroot --disable-intl --disable-libtool --disable-werror | tee $LOGFILE 2>&1
