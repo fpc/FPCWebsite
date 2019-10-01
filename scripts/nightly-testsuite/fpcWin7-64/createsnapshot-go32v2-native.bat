f2887d7b6ad8958d5de199a59cf602397e816c80
pierre <pierre@freepascal.org>
2019-10-01 21:16:18 +0000

    Add BUILDFULLNATIVE=1 to go32v2 native snapshot
    git-svn-id: @1516 -

--- scripts/nightly-testsuite/fpcWin7-64/createsnapshot-go32v2-native.bat
+++ scripts/nightly-testsuite/fpcWin7-64/createsnapshot-go32v2-native.bat
@@ -1,4 +1,4 @@
 @echo off
-set FPCLOG=go32v2-log.txt
-set MAKECMD=make distclean distclean singlezipinstall OS_TARGET=go32v2
+set FPCLOG=go32v2-native-log.txt
+set MAKECMD=make distclean distclean singlezipinstall OS_TARGET=go32v2 BUILDFULLNATIVE=1
 call create-common.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
