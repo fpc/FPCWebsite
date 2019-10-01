43341eb0ad912e65acca8fb054e33c69661c9c2c
pierre <pierre@freepascal.org>
2019-10-01 21:11:27 +0000

    Use createsnapshot-go32v2-native.bat instead of createsnapshot-go32v2.bat
    git-svn-id: @1515 -

--- scripts/nightly-testsuite/fpcWin7-64/createsnapshots.bat
+++ scripts/nightly-testsuite/fpcWin7-64/createsnapshots.bat
@@ -8,16 +8,16 @@ call setdate.bat
 set GLOGFILE=log-%START_DATE%.txt
 set GLOCKFILE=\pas\lock
 
-:waitlock
-if not exist %GLOCKFILE% goto lockok
+:wait_lock
+if not exist %GLOCKFILE% goto lock_ok
 call wait.bat 10
-goto waitlock
-:lockok
+goto wait_lock
+:lock_ok
 echo "Snapshot generation in progress for %WANTED_VERSION%" > %GLOCKFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gecho -n "Starting at " > %GLOGFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gdate +%%Y-%%m-%%d-%%H-%%M >> %GLOGFILE%
 call createsnapshot-win32.bat >> %GLOGFILE%
-call createsnapshot-go32v2.bat >> %GLOGFILE%
+call createsnapshot-go32v2-native.bat >> %GLOGFILE%
 call createsnapshot-win64.bat >> %GLOGFILE%
 call createsnapshots-msdos.bat >> %GLOGFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gecho -n "Ending at " >> %GLOGFILE%
