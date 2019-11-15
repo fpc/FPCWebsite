0529bfa0bc388dd0c6474e0564f344afb1eac5de
pierre <pierre@freepascal.org>
2019-11-15 07:20:39 +0000

    Correctly redirect stderr to log file
    git-svn-id: @1552 -

--- scripts/cross-binutils/do-one.sh
+++ scripts/cross-binutils/do-one.sh
@@ -230,7 +230,7 @@ function copytofpcbin ()
   if [ -z "$copy_only" ]; then
     export "$LOCAL_CFLAGS"
     export LDFLAGS="${LDFLAGS}"
-    ../../${binutilsdir}/configure $config_option --target=$target --with-sysroot --disable-intl --disable-libtool --disable-werror | tee $LOGFILE 2>&1
+    ../../${binutilsdir}/configure $config_option --target=$target --with-sysroot --disable-intl --disable-libtool --disable-werror 2>&1 | tee $LOGFILE
     configres=$?
     if [ $configres -ne 0 ] ; then
       echo "Configure failed, trying to erase build directory content before second try"
@@ -238,10 +238,10 @@ function copytofpcbin ()
       rm -Rf ${prefix}
       mkdir -p ${prefix}
       cd ${prefix}
-      ../../${binutilsdir}/configure $config_option --target=$target --with-sysroot --disable-intl --disable-libtool --disable-werror | tee $LOGFILE 2>&1
+      ../../${binutilsdir}/configure $config_option --target=$target --with-sysroot --disable-intl --disable-libtool --disable-werror 2>&1 | tee $LOGFILE
       configres=$?
     fi
-    make all-binutils all-gas all-ld | tee -a $LOGFILE 2>&1
+    make all-binutils all-gas all-ld 2>&1 | tee -a $LOGFILE
   fi
   if [ -d gas ] ; then
     cd gas
