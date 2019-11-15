016692ffa7b1616bb4cd9ec3ae810b74506b37bf
pierre <pierre@freepascal.org>
2019-11-15 15:20:47 +0000

    Also add automatic update of scripts directory for i386-linux nightly cronjobs
    git-svn-id: @1556 -

--- scripts/nightly-testsuite/i386-linux/linux-fpcallup.sh
+++ scripts/nightly-testsuite/i386-linux/linux-fpcallup.sh
@@ -6,5 +6,31 @@ export FIXES=0
  $HOME/bin/linux-fpccommonup.sh
  $HOME/bin/makesnapshottrunk.sh
 
+if [ -d $HOME/scripts ] ; then
+  SCRIPTDIR=$HOME/scripts
+elif [ -d $HOME/pas/scripts ] ; then
+  SCRIPTDIR=$HOME/pas/scripts
+else
+  SCRIPTDIR=
+fi
+# Check if script directory exists
+SVNLOGFILE=$HOME/logs/svn-scripts.log
+if [ -n "$SCRIPTDIR" ] ; then
+  cd $SCRIPTDIR
+  svn cleanup > $SVNLOGFILE 2>&1
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+else
+  if [ ! -d $HOME/pas/scripts ] ; then
+    cd $HOME/pas
+    svn checkout https://svn.freepascal.org/svn/html/scripts > $SVNLOGFILE 2>&1
+  else
+    cd $HOME/pas/scripts
+    svn cleanup > $SVNLOGFILE 2>&1
+  fi
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+fi
+
 $HOME/bin/test-go32v2.sh
 $HOME/bin/test-msdos.sh
