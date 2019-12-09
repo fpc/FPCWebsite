d30a659b832e6345d147df8429574fe228e2147e
pierre <pierre@freepascal.org>
2019-12-09 13:48:14 +0000

    Automatically update scripts directory
    git-svn-id: @1577 -

--- scripts/nightly-testsuite/aarch64-linux/fpcup.sh
+++ scripts/nightly-testsuite/aarch64-linux/fpcup.sh
@@ -32,3 +32,32 @@ elif [ -d "$HOME/sys-root/arm-linux/usr/arm-linux-$ARM_ABI/lib" ] ; then
 fi
 $HOME/bin/fpctrunkup.sh
 $HOME/bin/fpcfixesup.sh
+
+# Check if script directory exists
+SVNLOGFILE=$HOME/logs/svn-scripts.log
+
+if [ -d $HOME/scripts ] ; then
+  SCRIPTDIR=$HOME/scripts
+elif [ -d $HOME/pas/scripts ] ; then
+  SCRIPTDIR=$HOME/pas/scripts
+else
+  SCRIPTDIR=
+fi
+
+if [ -n "$SCRIPTDIR" ] ; then
+  cd $SCRIPTDIR
+  svn cleanup > $SVNLOGFILE 2>&1
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+else
+  if [ ! -d $HOME/pas/scripts ] ; then
+    cd $HOME/pas
+    svn checkout https://svn.freepascal.org/svn/html/scripts > $SVNLOGFILE 2>&1
+  else
+    cd $HOME/pas/scripts
+    svn cleanup > $SVNLOGFILE 2>&1
+  fi
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+fi
+
