a14fc766a96ec507541f722df26c8fb03192dbde
pierre <pierre@freepascal.org>
2019-12-29 17:16:38 +0000

    + Add skip and skipinstall options  + Add decho function and use it instead of echo  + Try also to compile rtl/packages/utils separately
    git-svn-id: @1585 -

--- scripts/nightly-testsuite/mips-linux/new-fullcycle.sh
+++ scripts/nightly-testsuite/mips-linux/new-fullcycle.sh
@@ -15,8 +15,24 @@ if [ "X$1" == "Xskiptrunk" ] ; then
 else
   skiptrunk=0
 fi
- 
+
+if [ "$1" == "skip" ] ; then
+  skip=1
+  shift
+else
+  skip=0
+fi
+
+if [ "$1" == "skipinstall" ] ; then
+  skipinstall=1
+  shift
+else
+  skipinstall=0
+fi
+
 . $HOME/bin/fpc-versions.sh
+
+set -u
 
 if [ -d $HOME/bin ] ; then
   export PATH=$HOME/bin:$PATH
@@ -28,8 +44,13 @@ export TMPDIR=$TMP
 
 ulimit -t 3600
 
-LOCKFILE=/home/fpcdevel/pas/lock
-OLDLOCKFILE=/home/fpcdevel/pas/last-lock
+LOCKFILE=$HOME/pas/lock
+OLDLOCKFILE=$HOME/pas/last-lock
+
+function decho ()
+{
+  echo "`date +%Y-%m-%d-%H:%M`: $*"
+}
 
 ENDIAN=`readelf -h /bin/sh | grep endian`
 MACHINE=`uname -n`
@@ -40,19 +61,19 @@ function run_testsuite {
   if [ "${localtestslog}" = "" ] ; then
     localtestslog=$testslog
   fi
-  echo "Using FPC=\"$TEST_FPC\" for tests"
-  echo "Using TEST_OPT=\"$TEST_OPT\" for tests"
-  echo "Using FPC=\"$TEST_FPC\" for tests" >> $localtestslog
-  echo "Using TEST_OPT=\"$TEST_OPT\" for tests" >> $localtestslog
+  decho "Using FPC=\"$TEST_FPC\" for tests"
+  decho "Using TEST_OPT=\"$TEST_OPT\" for tests"
+  decho "Using FPC=\"$TEST_FPC\" for tests" >> $localtestslog
+  decho "Using TEST_OPT=\"$TEST_OPT\" for tests" >> $localtestslog
   make -C ../rtl distclean > /dev/null
   make -C ../packages distclean > /dev/null
   make TEST_FPC="$TEST_FPC" TEST_OPT="$TEST_OPT" distclean allexectests \
     uploadrun DB_SSH_EXTRA="-i ~/.ssh/freepascal" 1>> $localtestslog 2>&1
   res=$?
-  echo "Finished tests, res=$res"
-  echo "Finished tests, res=$res" >> $localtestslog
+  decho "Finished tests, res=$res"
+  decho "Finished tests, res=$res" >> $localtestslog
   if [ $res -ne 0 ]; then
-    echo "Tests failed $res"
+    decho "Tests failed $res"
     attach="$attach -a ${localtestslog}"
   fi
 }
@@ -72,16 +93,16 @@ svnup_option="--accept=theirs-conflict"
 {
 echo "Start date `date +%Y-%m-%d-%H-%M`"
 
-echo "ENDIAN is $ENDIAN, MACHINE is $MACHINE"
+decho "ENDIAN is $ENDIAN, MACHINE is $MACHINE"
 
 if [ "${ENDIAN//big/}" != "${ENDIAN}" ] ; then
-  echo "Big endian machine"
+  decho "Big endian machine"
   # fpcmips32 machine
   ENDIAN=big
   FPCEXE=ppcmips
   export LANG=en_US.utf8
 else
-  echo "Little endian machine"
+  decho "Little endian machine"
   ENDIAN=little
   FPCEXE=ppcmipsel
   if [ "${MACHINE}" = "n16" ] ; then
@@ -95,10 +116,10 @@ else
   fi
 fi
 if [ "${PATH//pas\/fpc/}" == "${PATH}" ] ; then
-  echo "Adding $FPCBASEDIR/bin to PATH"
+  decho "Adding $FPCBASEDIR/bin to PATH"
   export PATH=$FPCBASEDIR/bin:~/bin:$PATH
 else
-  echo "Start PATH is $PATH"
+  decho "Start PATH is $PATH"
 fi
 
 if [ "X$RELEASEVERSION" != "X" ] ; then
@@ -115,25 +136,26 @@ cd $srcdir/fpcsrc/compiler
 
 
 FPCBIN=`which $FPCEXE`
-echo "Using binary $FPCBIN"
+decho "Using binary $FPCBIN"
 
-echo "Date before cycle `date +%Y-%m-%d-%H-%M`"
-if [ "$1" == "skip" ] ; then
-  echo "Skipping cycle"
-  shift
+decho "Date before cycle `date +%Y-%m-%d-%H-%M`"
+if [ $skip -eq 1 ] ; then
+  decho "Skipping cycle"
 else
-  echo "Starting cycle"
+  decho "Starting cycle"
   {
   make distclean > /dev/null
   make cycle DEBUG=1 FPC=$FPCBIN OPT=-n
   res=$?
   if [ $res -ne 0 ] ; then
-    echo "Cycle failed, res=$res, trying again with $FPCRELEASEBIN"
+    decho "Cycle failed, res=$res, trying again with $FPCRELEASEBIN"
     make distclean cycle DEBUG=1 FPC=$FPCRELEASEBIN OPT=-n
     res=$?
+  else
+    decho "Cycle finished successfully"
   fi
   if [ $res -ne 0 ] ; then
-    echo "Cycle with release fpc failed, res=$res"
+    decho "Cycle with release fpc failed, res=$res"
   else
     make install DEBUG=1 FPC=`pwd`/$FPCEXE OPT=-n PREFIX=$FPCBASEDIR
     res=$?
@@ -145,16 +167,15 @@ else
 fi
 
 if [ $res -ne 0 ] ; then
-  echo "Starting cycle failed"
+  decho "Starting cycle failed"
   attach="-a $cyclelog"
 else
   export PATH="$HOME/pas/fpc-$FPCVERSION/bin:$PATH"
   FPCBIN=`which $FPCEXE`
-  echo "Using new binary $FPCBIN"
+  decho "Using new binary $FPCBIN"
   cd ..
-  if [ "$1" == "skipinstall" ] ; then
-    echo "Skipping install"
-    shift
+  if [ $skipinstall -eq 1 ] ; then
+    decho "Skipping install"
   else
     repeat=1
     # -O2 option is still buggy, we need to use FPCCPUOPT=-O-, OBSOLETE?
@@ -162,21 +183,35 @@ else
     while [ $repeat -eq 1 ] ; do
       make distclean > /dev/null
       ADDOPT="FPCCPUOPT=$FPCCPUOPT"
-      echo "Starting make all install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR $ADDOPT OPT=-n OVERRIDEVERSIONCHECK=1 1> $alllog 2>&1"
+      decho "Starting make all install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR $ADDOPT OPT=-n OVERRIDEVERSIONCHECK=1 1> $alllog 2>&1"
       make all install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR $ADDOPT OPT=-n OVERRIDEVERSIONCHECK=1 1> $alllog 2>&1
       res=$?
+      allres=$res
       if [ $res -ne 0 ] ; then
-        echo "WARNING: make all failed, trying by sub-directories"
-        for dir in rtl packages utils ; do
-          echo "Starting make -C $dir install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR OPT="-n $FPCCPUOPT" OVERRIDEVERSIONCHECK=1 1>> $alllog 2>&1"
-          make -C $dir install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR OPT="-n $FPCCPUOPT" OVERRIDEVERSIONCHECK=1 1>> $alllog 2>&1
-          res=$?
+        decho "WARNING: make all failed, trying by sub-directories"
+        for dir in rtl packages packages/ide utils ; do
+          trial=1
+          max_trial=5
+          while [ $trial -lt $max_trial ] ; do
+            decho "Starting make -C $dir install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR OPT="-n $FPCCPUOPT" OVERRIDEVERSIONCHECK=1 1>> $alllog 2>&1"
+            make -C $dir install DEBUG=1 FPC=$FPCBIN PREFIX=$FPCBASEDIR OPT="-n $FPCCPUOPT" OVERRIDEVERSIONCHECK=1 1>> $alllog 2>&1
+            res=$?
+            if [ $res -ne 0 ] ; then
+              decho "make install in $dir failed, res=$res, retrying"
+              let trial++
+            else
+              let trial=$max_trial
+            fi
+          done
           if [ $res -ne 0 ] ; then
-            echo "make install in $dir failed, res=$res"
+            decho "make -C $dir failed $trial times"
+            let allres++
+          else
+            decho "make -C $dir succeeded after $trial trials"
           fi
         done
       fi
-      if [ $res -eq 0 ] ; then
+      if [ $allres -eq 0 ] ; then
         repeat=0
       else
         if [ "$FPCCPUOPT" == "-O2" ] ; then
@@ -192,32 +227,34 @@ else
       attach="$attach -a $alllog"
     fi
   fi
+  if [ $res -eq 0 ] ; then
   cd ./tests
-  { 
-  echo "Date before tests `date +%Y-%m-%d-%H-%M`"
-  ulimit -t 999
-  export FPC=$FPCBIN
-  export TEST_USER=muller
-  LIBGCC_PATH=`gcc -print-libgcc-file-name`
-  LIBGCC_DIR=`dirname $LIBGCC_PATH`
-  if [ "$LIBGCC_DIR" != "" ] ; then
-    export TEST_OPT="$TEST_OPT -Fl${LIBGCC_DIR}"
+    { 
+    echo "Date before tests `date +%Y-%m-%d-%H-%M`"
+    ulimit -t 999
+    export FPC=$FPCBIN
+    export TEST_USER=muller
+    LIBGCC_PATH=`gcc -print-libgcc-file-name`
+    LIBGCC_DIR=`dirname $LIBGCC_PATH`
+    if [ "$LIBGCC_DIR" != "" ] ; then
+      export TEST_OPT="$TEST_OPT -Fl${LIBGCC_DIR}"
+    fi
+    BASE_TEST_OPT="$TEST_OPT"
+    TEST_FPC=~/pas/fpc-$FPCVERSION/bin/$FPCEXE
+    # rm -f ${testslog}-default
+    # run_testsuite ${testslog}-default
+    TEST_OPT="$BASE_TEST_OPT -Cg"
+    rm -f ${testslog}-Cg
+    run_testsuite ${testslog}-Cg
+    TEST_OPT="$BASE_TEST_OPT -O1"
+    # rm -f ${testslog}-O1
+    # run_testsuite ${testslog}-O1
+    TEST_OPT="$BASE_TEST_OPT -O2"
+    rm -f ${testslog}-O2
+    run_testsuite ${testslog}-O2
+    echo "Tests finished";
+    }
   fi
-  BASE_TEST_OPT="$TEST_OPT"
-  TEST_FPC=~/pas/fpc-$FPCVERSION/bin/$FPCEXE
-  # rm -f ${testslog}-default
-  # run_testsuite ${testslog}-default
-  TEST_OPT="$BASE_TEST_OPT -Cg"
-  rm -f ${testslog}-Cg
-  run_testsuite ${testslog}-Cg
-  TEST_OPT="$BASE_TEST_OPT -O1"
-  # rm -f ${testslog}-O1
-  # run_testsuite ${testslog}-O1
-  TEST_OPT="$BASE_TEST_OPT -O2"
-  rm -f ${testslog}-O2
-  run_testsuite ${testslog}-O2
-  echo "Tests finished";
-  }
 fi;
 echo "Date at end `date +%Y-%m-%d-%H-%M`"
 } 1> $log 2>&1
