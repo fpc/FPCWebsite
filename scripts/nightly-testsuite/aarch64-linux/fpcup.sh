f16baa1836a30e4f1f2c7095d561a3cd2da6fb98
pierre <pierre@freepascal.org>
2019-12-29 17:18:03 +0000

    Also enable arm snapshot on gcc115
    git-svn-id: @1586 -

--- scripts/nightly-testsuite/aarch64-linux/fpcup.sh
+++ scripts/nightly-testsuite/aarch64-linux/fpcup.sh
@@ -9,7 +9,7 @@ fi
 
 if [ "$HOSTNAME" == "gcc115" ] ; then
   export generate_snapshots=1
-  export generate_arm_snapshots=0
+  export generate_arm_snapshots=1
   export ARM_ABI=gnueabihf
   export REQUIRED_ARM_OPT="-dFPC_ARMHF -Cparmv7a"
 elif [ "$HOSTNAME" == "gcc113" ] ; then
