99a216160d6a37735655a8edf736e8b6d1dc7db0
pierre <pierre@freepascal.org>
2020-04-06 20:29:49 +0000

    + Replace 'make' 'by '$MAKE' bash variable,     set MAKE variable if not already set, to     gmake if found in PATH, otherwise to make.   * Separate preparation part, i.e. recompilation of rtl and     full packages, which only needs to be done once at start.   + Add --skip-prepare, to allow skiiping the preparation phase.   * In compile_ide_with_libgdb, correctly copy all generated     units from gdbint and ide pacakges into a specific directory     called units-$release   * Replace long list of calls for different versions by     a list of all checked versions in a bash variable.
    git-svn-id: @1648 -

--- scripts/gdb/test-gdb-releases.sh
+++ scripts/gdb/test-gdb-releases.sh
@@ -6,44 +6,89 @@ if [ -z "$PASCALMAINDIR" ] ; then
   PASCALMAINDIR=$HOME/pas
 fi
 
+if [ -z "$MAKE" ] ; then
+  MAKE=`which gmake`
+  if [ -z "$MAKE" ] ; then
+    MAKE=`which make`
+  fi
+fi
+
+if [ "$1" == "--skip-prepare" ] ; then
+  skip_prepare=1
+  shift
+else
+  skip_prepare=0
+fi
+
+
+function prepare_compilation()
+{
+  if [ $skip_prepare -eq 1 ] ; then
+    echo "Skipping prepare_compilation"
+    return 0
+  fi
+  log=test-fp-with-gdb.log
+  echo "Preparing for IDE compilation with internal GDB"
+  echo "Calling '$MAKE distclean' in ../../packages and ../../rtl"
+  $MAKE -C ../../packages distclean OPT="-n -gl" > $log 2>&1
+  $MAKE -C ../../rtl distclean OPT="-n -gl" >> $log 2>&1
+
+  echo "Calling '$MAKE all' in ../../rtl"
+  $MAKE -C ../../rtl all OPT="-n -gl" >> $log 2>&1
+  res=$?
+  if [ $res -ne 0 ] ; then
+    echo "Error: Creation of rtl units failed, res=$res"
+    return 1
+  else
+    echo "Creation of rtl units OK"
+  fi
+
+  echo "Calling '$MAKE all' in ../../packages"
+  $MAKE -C ../../packages all DEBUG=1 >> $log 2>&1
+  res=$?
+  if [ $res -ne 0 ] ; then
+    echo "Error: Creation of packages units failed, res=$res"
+    return 1
+  else
+    echo "Creation of packages units OK"
+  fi
+}
+
 function compile_ide_with_libgdb ()
 {
   release=$1
   log=test-fp-${release}.log
+  export GDBLIBDIR=~/pas/libgdb/gdb-$release
 
-  if [ ! -d ~/pas/libgdb/gdb-$release ] ; then
-    echo "No libgdb directory for release $release"
+  if [ ! -f ~/pas/libgdb/gdb-$release/libgdb.a ] ; then
+    echo "No libgdb.a file in $GDBLIBDIR directory for release $release"
     return
+  else
+    echo "Testing libgdb.a file from $GDBLIBDIR directory for release $release"
   fi
 
-  export GDBLIBDIR=~/pas/libgdb/gdb-$release
+ echo "Testing ${release}" > $log
 
   if [ ! -d ./units-$release ] ; then
     mkdir ./units-$release
   fi
 
-  make -C ../../packages distclean OPT="-n -gl" > $log 2>&1
-  make -C ../../rtl distclean all OPT="-n -gl" >> $log 2>&1
-  res=$?
-  if [ $res -ne 0 ] ; then
-    echo "Error: Creation of rtl units failed, res=$res"
-    return
-  else
-    echo "Creation of rtl units for fp-$release OK"
-  fi
-
-  make -C ../../packages all DEBUG=1 >> $log 2>&1
   rm -Rf ../../packages/gdbint/units
-  make -C ../../packages/gdbint distclean all DEBUG=1 OPT="-n -gl -vwnihc" FPMAKEOPT=-v >> $log 2>&1
+  echo "Calling '$MAKE -C ../../packages/gdbint distclean all DEBUG=1 OPT=\"-n -gl -vwnihc\" FPMAKEOPT=-v'"
+  $MAKE -C ../../packages/gdbint distclean all DEBUG=1 OPT="-n -gl -vwnihc" FPMAKEOPT=-v >> $log 2>&1
   res=$?
   if [ $res -ne 0 ] ; then
     echo "Error: Compilation of gdbint for fp-$release failed, res=$res"
     return
   else
     echo "Compilation of gdbint of fp-$release OK"
+    cp -rfp ../../packages/gdbint/units/* ./units-$release
   fi
 
-  make distclean all NOGDBMI=1 DEBUG=1 OPT="-n -gl -v0wnih -k-Map -kfp-$release.map -k--exclude-libs -kreadline,history,bfd -dUSE_FPBASENAME" \
+  echo "Calling '$MAKE distclean all NOGDBMI=1 DEBUG=1 OPT=\"-n -gl -v0wnih -k-Map -kfp-$release.map -k--exclude-libs -kreadline,history,bfd -dUSE_FPBASENAME\" \
+    FPBASENAME=fp-$release FPMAKEOPT=-v'"
+
+  $MAKE distclean all NOGDBMI=1 DEBUG=1 OPT="-n -gl -v0wnih -k-Map -kfp-$release.map -k--exclude-libs -kreadline,history,bfd -dUSE_FPBASENAME" \
     FPBASENAME=fp-$release FPMAKEOPT=-v >> $log 2>&1
   res=$?
   if [ $res -ne 0 ] ; then
@@ -51,6 +96,7 @@ function compile_ide_with_libgdb ()
     return
   else
     echo "Make call for fp-$release OK"
+    cp -rfp ./units/* ./units-$release
   fi
 
   OS_TARGET=`fpc -iTO`
@@ -65,68 +111,27 @@ function compile_ide_with_libgdb ()
   fi
 }
 
-
 libgdb_dir_list=`ls -1d $PASCALMAINDIR/libgdb/ | sed "s:gdb-::" `
 
 if [ -n "$1" ] ; then
+  prepare_compilation
   for arg in $* ; do
     compile_ide_with_libgdb $arg
   done
-  exit
-fi
+else
+  (
+  prepare_compilation
+  gdb_release_list="4.18 5.0 5.1.0.1 5.1.1 5.1 5.2.1 5.2 5.3"
+  gdb_release_list+=" 6.0 6.1 6.1.1 6.2 6.2.1 6.3 6.4 6.5 6.6 6.7 6.7.1 6.8"
+  gdb_release_list+=" 7.0 7.0.1 7.1 7.2 7.2 7.3 7.3.1 7.4 7.4.1 7.5 7.5.1 7.6 7.6.1 7.6.2"
+  gdb_release_list+=" 7.7 7.7.1 7.8 7.8.1 7.8.2 7.9 7.9.1 7.10 7.10.1 7.11 7.11.1 7.12 7.12.1"
+  for ver in $gdb_release_list ; do
+    compile_ide_with_libgdb $ver
+  done
+  ) | tee  all.log 2>&1
 
-(
-compile_ide_with_libgdb 4.18
-compile_ide_with_libgdb 5.0
-compile_ide_with_libgdb 5.1.0.1
-compile_ide_with_libgdb 5.1.1
-compile_ide_with_libgdb 5.1
-compile_ide_with_libgdb 5.2.1
-compile_ide_with_libgdb 5.2
-compile_ide_with_libgdb 5.3
-compile_ide_with_libgdb 6.0
-compile_ide_with_libgdb 6.1
-compile_ide_with_libgdb 6.1.1
-compile_ide_with_libgdb 6.2
-compile_ide_with_libgdb 6.2.1
-compile_ide_with_libgdb 6.3
-compile_ide_with_libgdb 6.4
-compile_ide_with_libgdb 6.5
-compile_ide_with_libgdb 6.6
-compile_ide_with_libgdb 6.7
-compile_ide_with_libgdb 6.7.1
-compile_ide_with_libgdb 6.8
-compile_ide_with_libgdb 7.0
-compile_ide_with_libgdb 7.0.1
-compile_ide_with_libgdb 7.1
-compile_ide_with_libgdb 7.2
-compile_ide_with_libgdb 7.2
-compile_ide_with_libgdb 7.3
-compile_ide_with_libgdb 7.3.1
-compile_ide_with_libgdb 7.4
-compile_ide_with_libgdb 7.4.1
-compile_ide_with_libgdb 7.5
-compile_ide_with_libgdb 7.5.1
-compile_ide_with_libgdb 7.6
-compile_ide_with_libgdb 7.6.1
-compile_ide_with_libgdb 7.6.2
-
-compile_ide_with_libgdb 7.7
-compile_ide_with_libgdb 7.7.1
-compile_ide_with_libgdb 7.8
-compile_ide_with_libgdb 7.8.1
-compile_ide_with_libgdb 7.8.2
-compile_ide_with_libgdb 7.9
-compile_ide_with_libgdb 7.9.1
-compile_ide_with_libgdb 7.10
-compile_ide_with_libgdb 7.10.1
-compile_ide_with_libgdb 7.11
-compile_ide_with_libgdb 7.11.1
-compile_ide_with_libgdb 7.12
-compile_ide_with_libgdb 7.12.1
-) | tee  all.log 2>&1
-
-if [ ! -z "$MAILTO" ] ; then
-  # pierre@freepascal.org
-  mutt -x -s "Test FP IDE libgdb versions" -i all.log -- $MAILTO  < /dev/null
+  if [ ! -z "$MAILTO" ] ; then
+    # pierre@freepascal.org
+    mutt -x -s "Test FP IDE libgdb versions" -i all.log -- $MAILTO  < /dev/null
+  fi
 fi
