51b0ce8008dcd2720aad1f9ee4a9819176fa9f65
pierre <pierre@freepascal.org>
2020-05-02 13:21:52 +0000

    * Change default version to 5.0.0   + Add support for a patch file
    git-svn-id: @1660 -

--- scripts/cross-compile/install-qemu.sh
+++ scripts/cross-compile/install-qemu.sh
@@ -2,7 +2,7 @@
 
 QEMU_VERSION="$1"
 if [ -z "$QEMU_VERSION" ] ; then
-  QEMU_VERSION=4.2.0
+  QEMU_VERSION=5.0.0
 fi
 if [ ! -f qemu-$QEMU_VERSION.tar.xz ] ; then
   echo "Downloading qemu version $QEMU_VERSION"
@@ -14,19 +14,34 @@ fi
 
 echo "Unpacking qemu version $QEMU_VERSION"
 tar xvJf qemu-$QEMU_VERSION.tar.xz
+if [ -f qemu-$QEMU_VERSION.patch ] ; then
+  cd qemu-$QEMU_VERSION
+  echo "Applying patch ../qemu-$QEMU_VERSION.patch"
+  patch -p 1 -i ../qemu-$QEMU_VERSION.patch
+  res=$?
+  if [ $res -ne 0 ] ; then
+    echo "patch failed to apply cleanly, res=$res"
+    cd ..
+    exit
+  fi
+  cd ..
+fi
+
 if [ -d build-qemu-$QEMU_VERSION ] ; then
   rm -Rf build-qemu-$QEMU_VERSION
 fi
 mkdir build-qemu-$QEMU_VERSION
 cd build-qemu-$QEMU_VERSION
-echo "Configuring qemu version $QEMU_VERSION"
 export CFLAGS="-gdwarf-4 -I$HOME/gnu/include -L$HOME/gnu/lib" 
+echo "Configuring qemu version $QEMU_VERSION, using CFLAGS=\"$CFLAGS\""
 ../qemu-$QEMU_VERSION/configure --prefix=$HOME/sys-root --enable-curses --extra-cflags="$CFLAGS"
 echo "Making qemu version $QEMU_VERSION"
-make
+make > make-$QEMU_VERSION.log 2>&1
 res=$?
-
+cd ..
 if [ $res -eq 0 ] ; then
-  echo "Make finished OK for QEMU version $QEMU_VERSION, you can install using \"make install\""
+  echo "Make finished OK for QEMU version $QEMU_VERSION, you can install using \"make -C ./qemu-$QEMU_VERSION install\""
+else
+  echo "make failed, res=$res, details are in ./qemu-$QEMU_VERSION/make-$QEMU_VERSION.log file"
 fi
 
