50211a37dccd97cf5dcfd6f7991c7d5827e30e98
pierre <pierre@freepascal.org>
2020-07-12 15:33:52 +0000

    Separate cycle from fullcycle
    git-svn-id: @1787 -

--- scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
+++ scripts/nightly-testsuite/fpc-sparc64/linux-fpcallup.sh
@@ -82,9 +82,10 @@ fi
 export FIXES=1
 cd $HOME/pas/fixes/fpcsrc/compiler
 # regenerate native compiler for check-all-rtl.sh script, as the last one might be too old
-echo "`date +%Y-%m-%d-%H:%M`: regenerate cross-compilers for fixes check-all-rtl.sh script" >> $GLOGFILE
-make distclean cycle installsymlink INSTALL_PREFIX=$HOME/pas/fpc-$FIXESVERSION FPC=$HOME/pas/fpc-$FIXESVERSION/bin/ppcsparc > $HOME/logs/fixes-fullinstall.log 2>&1
+echo "`date +%Y-%m-%d-%H:%M`: regenerate sparc compiler for fixes check-all-rtl.sh script" >> $GLOGFILE
+make distclean cycle installsymlink INSTALL_PREFIX=$HOME/pas/fpc-$FIXESVERSION FPC=$HOME/pas/fpc-$FIXESVERSION/bin/ppcsparc > $HOME/logs/fixes-installsymlink.log 2>&1
 # regenerate cross-compilers for check-all-rtl.sh script
+echo "`date +%Y-%m-%d-%H:%M`: regenerate cross-compilers for fixes check-all-rtl.sh script" >> $GLOGFILE
 make fullcycle fullinstallsymlink INSTALL_PREFIX=$HOME/pas/fpc-$FIXESVERSION FPC=$HOME/pas/fpc-$FIXESVERSION/bin/ppcsparc >> $HOME/logs/fixes-fullinstall.log 2>&1
 # Update ppc386, ppcx64 and ppc8086 with softfpu extended emulation
 echo "`date +%Y-%m-%d-%H:%M`:  Update fixes ppc386, ppcx64 and ppc8086 with softfpu extended emulation" >> $GLOGFILE
