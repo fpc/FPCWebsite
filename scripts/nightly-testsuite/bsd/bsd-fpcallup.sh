d604a0e8029d7a7bc43aba8678961caeffa9d5a0
pierre <pierre@freepascal.org>
2021-01-25 22:38:35 +0000

    Add automatic update of scripts directory
    git-svn-id: @1993 -

--- scripts/nightly-testsuite/bsd/bsd-fpcallup.sh
+++ scripts/nightly-testsuite/bsd/bsd-fpcallup.sh
@@ -71,4 +71,31 @@ if [ -f "$SVN" ] ; then
   fi
 fi
 
+if [ -d $HOME/scripts ] ; then
+  SCRIPTDIR=$HOME/scripts
+elif [ -d $HOME/pas/scripts ] ; then
+  SCRIPTDIR=$HOME/pas/scripts
+else
+  SCRIPTDIR=
+fi
+
+# Check if script directory exists
+SVNLOGFILE=$HOME/logs/svn-scripts.log
+if [ -n "$SCRIPTDIR" ] ; then
+  cd $SCRIPTDIR
+  svn cleanup > $SVNLOGFILE 2>&1
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+else
+  if [ ! -d $HOME/pas/scripts ] ; then
+    cd $HOME/pas
+    svn checkout https://svn.freepascal.org/svn/html/scripts > $SVNLOGFILE 2>&1
+  else
+    cd $HOME/pas/scripts
+    svn cleanup > $SVNLOGFILE 2>&1
+  fi
+  svn up --non-interactive --accept theirs-conflict >> $SVNLOGFILE 2>&1 
+  cd $HOME
+fi
+
 
