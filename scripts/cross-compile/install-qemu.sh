c0d12595ed5f2b086f4c4b4138a51f7b27bc780b
pierre <pierre@freepascal.org>
2021-02-08 11:01:00 +0000

    + Add QEMU_NAME variable.   + Check that wget succeeds, exit if it fails to download qemu tar.xz file   * Move old directory $QEMU_NAME to prev-$QEMU_NAME   + Handle $QEMU_NAME.patch if present, by copying     $QEMU_NAME directory to ${QEMU_NAME}-ori directory     and applying patch to $QEMU_NAME directory.
    git-svn-id: @2014 -

--- scripts/cross-compile/install-qemu.sh
+++ scripts/cross-compile/install-qemu.sh
@@ -12,22 +12,33 @@ if [ ! -d $HOME/gnu/qemu ] ; then
   mkdir -p $HOME/gnu/qemu
 fi
 
+QEMU_NAME=qemu-$QEMU_VERSION
+
 cd $HOME/gnu/qemu
 
-if [ ! -f qemu-$QEMU_VERSION.tar.xz ] ; then
+if [ ! -f $QEMU_NAME.tar.xz ] ; then
   echo "Downloading qemu version $QEMU_VERSION"
-  wget https://download.qemu.org/qemu-$QEMU_VERSION.tar.xz
+  wget https://download.qemu.org/$QEMU_NAME.tar.xz
+  res=$?
+  if [ $res -ne 0 ] ; then
+    echo "Failed to download $QEMU_NAME.tar.xz"
+    exit
+  fi
 fi
-if [ -d qemu-$QEMU_VERSION ] ; then
-  mv -f qemu-$QEMU_VERSION prev-qemu-$QEMU_VERSION
+if [ -d $QEMU_NAME ] ; then
+  mv -f $QEMU_NAME prev-$QEMU_NAME
 fi
 
 echo "Unpacking qemu version $QEMU_VERSION"
-tar xvJf qemu-$QEMU_VERSION.tar.xz > tar-qemu-$QEMU_VERSION.log
-if [ -f qemu-$QEMU_VERSION.patch ] ; then
-  cd qemu-$QEMU_VERSION
-  echo "Applying patch ../qemu-$QEMU_VERSION.patch"
-  patch -p 1 -i ../qemu-$QEMU_VERSION.patch
+tar xvJf $QEMU_NAME.tar.xz > tar-$QEMU_NAME.log
+if [ -f $QEMU_NAME.patch ] ; then
+  if [ -d $QEMU_NAME-ori ] ; then
+    rm -Rf qemu_$QEMU_VERSION-ori
+  fi
+  cp -Rfp $QEMU_NAME $QEMU_NAME-ori
+  cd $QEMU_NAME
+  echo "Applying patch ../$QEMU_NAME.patch"
+  patch -p 1 -i ../$QEMU_NAME.patch
   res=$?
   if [ $res -ne 0 ] ; then
     echo "patch failed to apply cleanly, res=$res"
@@ -35,23 +46,25 @@ if [ -f qemu-$QEMU_VERSION.patch ] ; then
     exit
   fi
   cd ..
+  diff -urN $QEMU_NAME-ori $QEMU_NAME > $QEMU_NAME-check.patch
 fi
 
-if [ -d build-qemu-$QEMU_VERSION ] ; then
-  rm -Rf build-qemu-$QEMU_VERSION
+if [ -d build-$QEMU_NAME ] ; then
+  rm -Rf build-$QEMU_NAME
 fi
-mkdir build-qemu-$QEMU_VERSION
-cd build-qemu-$QEMU_VERSION
-export CFLAGS="-gdwarf-4 -I$HOME/gnu/include -L$HOME/gnu/lib" 
+mkdir build-$QEMU_NAME
+cd build-$QEMU_NAME
+export CFLAGS="-gdwarf-4"
+#  -I$HOME/gnu/include -L$HOME/gnu/lib" 
 echo "Configuring qemu version $QEMU_VERSION, using CFLAGS=\"$CFLAGS\""
-../qemu-$QEMU_VERSION/configure --prefix=$HOME/sys-root --enable-curses --extra-cflags="$CFLAGS"
+../$QEMU_NAME/configure --prefix=$HOME/sys-root --enable-curses --extra-cflags="$CFLAGS"
 echo "Making qemu version $QEMU_VERSION"
 make $MAKE_OPT > make-$QEMU_VERSION.log 2>&1
 res=$?
 cd ..
 if [ $res -eq 0 ] ; then
-  echo "Make finished OK for QEMU version $QEMU_VERSION, you can install using \"make -C ./build-qemu-$QEMU_VERSION install\""
+  echo "Make finished OK for QEMU version $QEMU_VERSION, you can install using \"make -C ./build-$QEMU_NAME install\""
 else
-  echo "make failed, res=$res, details are in ./qemu-$QEMU_VERSION/make-$QEMU_VERSION.log file"
+  echo "make failed, res=$res, details are in ./$QEMU_NAME/make-$QEMU_VERSION.log file"
 fi
 
