0a3c4a0b63e4c872f89e6b8743575ae060a6073f
pierre <pierre@freepascal.org>
2021-02-18 22:55:13 +0000

    Script renamed install-release.sh   + Version can be explicitly given as unique parameter.   + is_beta variable set, if rc is part of subversion tag/branch name.   * RELEASE renamed RELEASE_VERSION.
    git-svn-id: @2028 -

--- scripts/release/install-release.sh
+++ scripts/release/install-release.sh
@@ -4,19 +4,33 @@ cd $HOME/pas
 
 set -u
 
-RELEASE=3.2.0
+if [ -n "$1" ] ; then
+  RELEASE_VERSION=$1
+else
+  RELEASE_VERSION=3.2.2
+fi
+
+$FTP_MACHINE=fpcftp
+
+if ["${RELEASE__VERSION/rc/}" != "${RELEASE_VERSION}" ] ; then
+  is_beta=1
+  FTP_BASE_DIR=ftp/beta
+else
+  is_beta=0
+  FTP_BASE_DIR=ftp/dist
+fi
 
 OS=`uname -s | tr "[:upper:]" "[:lower:]" `
 CPU=`uname -m | tr "[:upper:]" "[:lower:]" `
 
-echo "Trying to install Free Pascal release $RELEASE on $CPU-$OS"
+echo "Trying to install Free Pascal release $RELEASE_VERSION on $CPU-$OS"
 
 CPU32=""
 CPU64=""
 
 case "$CPU" in
   i*86) CPU32=i386;;
-  ppc64le|pwerpc64le) CPU64=powerpc64le;;
+  ppc64le|powerpc64le) CPU64=powerpc64le;;
   ppc64|powerpc64) CPU32=powerpc; CPU64=powerpc64;;
   x86_64|amd64) CPU32=i386; CPU64=x86_64;;
   ppc*|powerpc*) CPU32=powerpc;;
@@ -33,14 +47,14 @@ esac
 
 if [ -n "$CPU32" ] ; then
   TARGET32=$CPU32-$OS
-  DIRNAME32=fpc-$RELEASE.$TARGET32
+  DIRNAME32=fpc-$RELEASE_VERSION.$TARGET32
   TARFILE32=$DIRNAME32.tar
-  scp -p fpcftp:ftp/dist/$RELEASE/$TARGET32/$TARFILE32 .
+  scp -p ${FTP_MACHINE}:$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET32/$TARFILE32 .
   res=$?
   if [ $res -ne 0 ] ; then
-    DIRNAME32=fpc-$RELEASE-$TARGET32
+    DIRNAME32=fpc-$RELEASE_VERSION-$TARGET32
     TARFILE32=$DIRNAME32.tar
-    scp -p fpcftp:ftp/dist/$RELEASE/$TARGET32/$TARFILE32 .
+    scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET32/$TARFILE32 .
     res=$?
   fi
   if [ $res -ne  0 ] ; then
@@ -50,19 +64,24 @@ fi
 
 if [ -n "$CPU64" ] ; then
   TARGET64=$CPU64-$OS
-  DIRNAME64=fpc-$RELEASE.$TARGET64
+  DIRNAME64=fpc-$RELEASE_VERSION.$TARGET64
   TARFILE64=$DIRNAME64.tar
-  scp -p fpcftp:ftp/dist/$RELEASE/$TARGET64/$TARFILE64 .
+  scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
   res=$?
   if [ $res -ne 0 ] ; then
-    DIRNAME64=fpc-$RELEASE-$TARGET64
+    DIRNAME64=fpc-$RELEASE_VERSION-$TARGET64
     TARFILE64=$DIRNAME64.tar
-    scp -p fpcftp:ftp/dist/$RELEASE/$TARGET64/$TARFILE64 .
+    scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
     res=$?
   fi
   if [ $res -ne 0 ] ; then
     CPU64=""
   fi
+fi
+
+if [[ ( -z "$CPU32" ) && ( -z "$CPU64" ) ]] ; then
+  echo "No installation file found in ${FTP_MACHINE}:$FTP_BASE_DIR"
+  exit
 fi
 
 function install_this ()
@@ -75,7 +94,7 @@ function install_this ()
     echo "Directory $installdir already exists, skipping"
     return
   fi
-  echo "Unpacking $tarfile"
+  echo "Unpacking $tarfile in $installdir"
   tar -xvf $tarfile 
   if [ ! -d "$dirname" ] ; then
     altdirname=${dirname/le/}
@@ -112,14 +131,14 @@ fi
 }
 
 if [[ ( -n "$CPU32" ) && ( -n "$CPU64" ) ]] ; then
-  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE-32
-  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE-64
+  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION-32
+  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION-64
 fi
 if [ -n "$CPU32" ] ; then
-  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE
+  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION
 fi
 if [ -n "$CPU64" ] ; then
-  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE force
+  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION force
 fi
 
 
