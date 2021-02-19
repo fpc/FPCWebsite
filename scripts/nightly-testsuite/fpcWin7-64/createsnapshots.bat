8160562d29a43fab43e1a7b495265408d0ccb4e2
pierre <pierre@freepascal.org>
2021-02-19 10:38:33 +0000

    Copy lock to lock.last at end of batch execution
    git-svn-id: @2032 -

--- scripts/nightly-testsuite/fpcWin7-64/createsnapshots.bat
+++ scripts/nightly-testsuite/fpcWin7-64/createsnapshots.bat
@@ -7,6 +7,7 @@ call setdate.bat
 
 set GLOGFILE=log-%START_DATE%.txt
 set GLOCKFILE=\pas\lock
+set LASTGLOCKFILE=\pas\lock.last
 
 :wait_lock
 if not exist %GLOCKFILE% goto lock_ok
@@ -14,6 +15,8 @@ call wait.bat 10
 goto wait_lock
 :lock_ok
 echo "Snapshot generation in progress for %WANTED_VERSION%" > %GLOCKFILE%
+echo "Started at:" >> %GLOCKFILE%
+e:\pas\fpc-3.0.4\bin\i386-win32\gdate +%%Y-%%m-%%d-%%H-%%M >> %GLOCKFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gecho -n "Starting at " > %GLOGFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gdate +%%Y-%%m-%%d-%%H-%%M >> %GLOGFILE%
 call createsnapshot-win32.bat >> %GLOGFILE%
@@ -22,5 +25,8 @@ call createsnapshot-win64.bat >> %GLOGFILE%
 call createsnapshots-msdos.bat >> %GLOGFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gecho -n "Ending at " >> %GLOGFILE%
 e:\pas\fpc-3.0.4\bin\i386-win32\gdate +%%Y-%%m-%%d-%%H-%%M >> %GLOGFILE%
+echo "Ended at:" >> %GLOCKFILE%
+e:\pas\fpc-3.0.4\bin\i386-win32\gdate +%%Y-%%m-%%d-%%H-%%M >> %GLOCKFILE%
 
+copy /y %GLOCKFILE% %LASTGLOCKFILE%
 del %GLOCKFILE%
