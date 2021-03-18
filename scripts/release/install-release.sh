18d38aad8044a3d31b5dd7ac817e43136cf2d9bb
pierre <pierre@freepascal.org>
2021-03-18 11:07:48 +0000

    Try to install both 32-bit and 64-bit versions of new release
    git-svn-id: @2058 -

--- scripts/release/install-release.sh
+++ scripts/release/install-release.sh
@@ -2,22 +2,28 @@
 
 cd $HOME/pas
 
-set -u
-
 if [ -n "$1" ] ; then
   RELEASE_VERSION=$1
 else
   RELEASE_VERSION=3.2.2
 fi
 
-$FTP_MACHINE=fpcftp
+set -u
 
-if ["${RELEASE__VERSION/rc/}" != "${RELEASE_VERSION}" ] ; then
+FTP_MACHINE=fpcftp
+
+if [ "${RELEASE_VERSION/-rc/}" != "${RELEASE_VERSION}" ] ; then
   is_beta=1
+  add_symlinks=1
   FTP_BASE_DIR=ftp/beta
+  RELEASE_BASE_VERSION=${RELEASE_VERSION/-rc*/}
+  RCPART=${RELEASE_VERSION/$RELEASE_BASE_VERSION/}
 else
   is_beta=0
+  add_symlinks=0
   FTP_BASE_DIR=ftp/dist
+  RELEASE_BASE_VERSION=$RELEASE_VERSION
+  RCPART=""
 fi
 
 OS=`uname -s | tr "[:upper:]" "[:lower:]" `
@@ -47,14 +53,14 @@ esac
 
 if [ -n "$CPU32" ] ; then
   TARGET32=$CPU32-$OS
-  DIRNAME32=fpc-$RELEASE_VERSION.$TARGET32
-  TARFILE32=$DIRNAME32.tar
+  DIRNAME32=fpc-${RELEASE_VERSION/-rc/rc}.$TARGET32
+  TARFILE32=${DIRNAME32}.tar
   scp -p ${FTP_MACHINE}:$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET32/$TARFILE32 .
   res=$?
   if [ $res -ne 0 ] ; then
-    DIRNAME32=fpc-$RELEASE_VERSION-$TARGET32
-    TARFILE32=$DIRNAME32.tar
-    scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET32/$TARFILE32 .
+    DIRNAME32=fpc-${RELEASE_VERSION/-rc/rc}-$TARGET32
+    TARFILE32=${DIRNAME32}.tar
+    scp -p ${FTP_MACHINE}:$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET32/$TARFILE32 .
     res=$?
   fi
   if [ $res -ne  0 ] ; then
@@ -64,14 +70,14 @@ fi
 
 if [ -n "$CPU64" ] ; then
   TARGET64=$CPU64-$OS
-  DIRNAME64=fpc-$RELEASE_VERSION.$TARGET64
-  TARFILE64=$DIRNAME64.tar
-  scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
+  DIRNAME64=fpc-${RELEASE_VERSION/-rc/rc}.$TARGET64
+  TARFILE64=${DIRNAME64}.tar
+  scp -p ${FTP_MACHINE}:$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
   res=$?
   if [ $res -ne 0 ] ; then
-    DIRNAME64=fpc-$RELEASE_VERSION-$TARGET64
-    TARFILE64=$DIRNAME64.tar
-    scp -p ${FTP_MACHINE}/$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
+    DIRNAME64=fpc-${RELEASE_VERSION/-rc/rc}-$TARGET64
+    TARFILE64=${DIRNAME64}.tar
+    scp -p ${FTP_MACHINE}:$FTP_BASE_DIR/$RELEASE_VERSION/$TARGET64/$TARFILE64 .
     res=$?
   fi
   if [ $res -ne 0 ] ; then
@@ -84,11 +90,21 @@ if [[ ( -z "$CPU32" ) && ( -z "$CPU64" ) ]] ; then
   exit
 fi
 
+# From log file:
+# Writing sample configuration file to /home/muller/.fpc.cfg
+# Writing sample configuration file to /home/muller/pas/fpc-3.2.2-rc1/lib/fpc/3.2.2/ide/text/fp.cfg
+# Writing sample configuration file to /home/muller/pas/fpc-3.2.2-rc1/lib/fpc/3.2.2/ide/text/fp.ini
+# Writing sample configuration file to /home/muller/.config/fppkg.cfg
+# Writing sample configuration file to /home/muller/.fppkg/config/default
+
+config_file_list="$HOME/.fpc.cfg $HOME/.config/fppkg.cfg $HOME/.fppkg/config/default"
+
 function install_this ()
 {
   tarfile="$1"
   dirname="$2"
   installdir="$3"
+  cpu="$4"
   force=${4:-}
   if [[ ( -d "$installdir" ) && ( -z "$force" ) ]] ; then
     echo "Directory $installdir already exists, skipping"
@@ -117,28 +133,57 @@ $installdir
 
 
 HERE
+
+for f in $config_file_list ; do
+  if [ -f "$f" ] ; then
+    mv -fv "$f" "$f-backup"
+  elif [ -d "$f" ] ; then
+    if [ -d "$f-dir_backup" ] ; then
+      rm -Rf "$f-dir_backup"
+    fi
+    mv -fv "$f" "$f-dir_backup"
+  fi
+done
+
+install_log=../install-$RELEASE_VERSION-$cpu.log
 echo "Installing into $installdir, using install.sh script"
-./install.sh < ./install-answers > ./install.log
+
+./install.sh < ./install-answers > $install_log
 res=$?
 if [ $res -ne 0 ] ; then
-  echo "install.sh failed, res=$res, log is in `pwd`/install.log"
+  echo "install.sh failed, res=$res, log is in $install_log"
   exit
 fi
 cd ..
 if [ $res -eq 0 ] ; then
   rm -Rf ${dirname}
 fi
+for f in $config_file_list ; do
+  if [[ ( -f "$f" ) || ( -d "$f" ) ]] ; then
+    mv -fv "$f" "$f-$RELEASE_VERSION-$cpu"
+  fi
+  if [ -f "$f-backup" ] ; then
+    mv -fv "$f-backup" "$f"
+  elif [ -d "$f-dir_backup" ] ; then
+    mv -fv "$f-dir_backup" "$f"
+  fi
+done
+if [ $add_symlinks -eq 1 ] ; then
+  ln -sf $installdir ${installdir/$RCPART/}
+fi
 }
 
+
+
 if [[ ( -n "$CPU32" ) && ( -n "$CPU64" ) ]] ; then
-  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION-32
-  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION-64
+  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION-32 $CPU32
+  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION-64 $CPU64
 fi
 if [ -n "$CPU32" ] ; then
-  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION
+  install_this $TARFILE32 $DIRNAME32 $HOME/pas/fpc-$RELEASE_VERSION $CPU32
 fi
 if [ -n "$CPU64" ] ; then
-  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION force
+  install_this $TARFILE64 $DIRNAME64 $HOME/pas/fpc-$RELEASE_VERSION $CPU64 force
 fi
 
 
