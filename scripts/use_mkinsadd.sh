c7c0d8f938d8cbbaa84b966e42a25d9334b3de5c
pierre <pierre@freepascal.org>
2021-03-28 21:58:17 +0000

    Yet another package tester using mkinsadd utility
    git-svn-id: @2069 -

---
+++ scripts/use_mkinsadd.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+. $HOME/bin/fpc-versions.sh
+
+if [ "$FIXES"  == "1" ] ; then
+  VERDIR=$FIXESDIR
+else
+  VERDIR=$TRUNKDIR
+fi
+
+
+MKINSADD=`which mkinsadd`
+
+if [ ! -f "$MKINSADD" ] ; then
+  echo "mkinsadd Free Pascal utility not found!"
+  exit 1
+fi
+
+pkg_file_list=""
+
+function check_target ()
+{
+  fpc_target_cpu=$1
+  fpc_target_os=$2
+  fpc_short_os=$3
+  dir=$4
+  dirname=`basename $dir`
+  if [ "$dirname" == "packages" ] ; then
+    zipprefix="--zipprefix=units-"
+  else
+    zipprefix=""
+  fi
+  $dir/fpmake pkglist --cpu=$fpc_target_cpu --target=$fpc_target_os $zipprefix
+  pkglist=pkg-${fpc_target_os}.lst
+  if [ -f "$pkglist" ] ; then
+    cp $pkglist ${pkglist}-$dirname
+    pkglist=${pkglist}-$dirname
+    pkg_file_list+=" ${pkglist}"
+  else
+    pkglist=pkg-${fpc_target_cpu}-${fpc_target_os}.lst
+    if [ -f "$pkglist" ] ; then
+      cp $pkglist ${pkglist}-$dirname
+      pkglist=${pkglist}-$dirname
+      pkg_file_list+=" ${pkglist}"
+    else
+      echo "not found"
+    fi
+  fi
+  if [ -f "$pkglist" ] ; then
+    sed -i "s:\.tar\.gz:.zip:g" $pkglist
+  fi
+}
+
+cd $VERDIR
+if [ -d fpcsrc ] ; then
+  cd fpcsrc
+  VERDIR=$VERDIR/fpcsrc
+fi
+
+check_target i386 go32v2 dos $VERDIR/packages
+check_target i386 go32v2 dos $VERDIR/utils
+
+check_target i386 emx emx $VERDIR/packages
+check_target i386 emx emx $VERDIR/utils
+
+check_target i386 os2 os2 $VERDIR/packages
+check_target i386 os2 os2 $VERDIR/utils
+
+check_target i386 win32 w32 $VERDIR/packages
+check_target i386 win32 w32 $VERDIR/utils
+
+$MKINSADD $VERDIR/installer/install.dat $pkg_file_list
+
+
