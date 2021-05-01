993d89084eb8d6b1bd4d935c3dfbac488f1ee2bc
pierre <pierre@freepascal.org>
2021-05-01 11:28:17 +0000

    Try to get this script to work both inside riscv64-linux VM and on x86_64-linux host machines
    git-svn-id: @2101 -

--- scripts/nightly-testsuite/riscv64-linux/comp-riscv64.sh
+++ scripts/nightly-testsuite/riscv64-linux/comp-riscv64.sh
@@ -20,8 +20,15 @@ OS_TARGET=`$TARGET_FPC -iTO`
 FULL_TARGET=$CPU_TARGET-$OS_TARGET
 GNU_TARGET_DIR="$FULL_TARGET-gnu"
 COMMON_TARGET_OPT=""
-TARGET_SYSROOT="$HOME/sys-root/$FULL_TARGET"
-if [ -d "$TARGET_SYSROOT" ] ; then
+if [ "$NATIVE_CPU" == "riscv64" ] ; then
+  TARGET_SYSROOT=""
+  native_risc=1
+else
+  TARGET_SYSROOT="$HOME/sys-root/$FULL_TARGET"
+  native_risc=0
+fi
+
+if [[ ( -z "$TARGET_SYSROOT" ) || ( -d "$TARGET_SYSROOT" ) ]] ; then
   if [ -d "$TARGET_SYSROOT/lib" ] ; then
     COMMON_TARGET_OPT+=" -Fl$TARGET_SYSROOT/lib"
     COMMON_TARGET_OPT+=" -k-rpath-link=$TARGET_SYSROOT/lib -k-L -k$TARGET_SYSROOT/lib"
@@ -42,13 +49,16 @@ if [ -d "$TARGET_SYSROOT" ] ; then
     COMMON_TARGET_OPT+=" -Fl$TARGET_SYSROOT/usr/lib/gcc/$GNU_TARGET_DIR/9"
     COMMON_TARGET_OPT+=" -k-rpath-link=$TARGET_SYSROOT/usr/lib/gcc/$GNU_TARGET_DIR/9 -k-L -k$TARGET_SYSROOT/usr/lib/gcc/$GNU_TARGET_DIR/9"
   fi
-  COMMON_TARGET_OPT+=" -Xd -k--sysroot=$TARGET_SYSROOT"
-  COMMON_TARGET_OPT="-k-nostdlib $COMMON_TARGET_OPT"
+  if [ $native_risc -eq 0 ] ; then
+    COMMON_TARGET_OPT+=" -Xd -XR$TARGET_SYSROOT -k--sysroot=$TARGET_SYSROOT"
+    COMMON_TARGET_OPT="-k-nostdlib $COMMON_TARGET_OPT"
+  fi
 fi
 
 TARGET_BINUTILSPREFIX=$FULL_TARGET-
 COMMON_TARGET_OPT+=" -XP$TARGET_BINUTILSPREFIX"
 export QEMU_LD_PREFIX=$HOME/sys-root/$FULL_TARGET/
+export LD_PRELOAD=
 echo "$TARGET_FPC -vx $COMMON_TARGET_OPT $*"
 $TARGET_FPC -vx $COMMON_TARGET_OPT $*
 
